65036b05760773b20799a991de5aaada9ad622f1
Anri Asaturov <anri82@gmail.com>
2016-10-11 17:29:03 +0300

    WIP: styles. Add fonts.

--- scripts/get-locales.sh
+++ scripts/get-locales.sh
@@ -1,3 +1,3 @@
-cp node_modules/peerio-copy/client_en.json locales/en.json 
-cp -R node_modules/peerio-copy/phrase/dict locales
+cp node_modules/peerio-copy/client_en.json static/locales/en.json 
+cp -R node_modules/peerio-copy/phrase/dict static/locales
 tx pull -af

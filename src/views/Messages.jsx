2c6c3958da197422da054ae517181beef6349031
paulrdukes <paul.r.dukes@gmail.com>
2016-10-30 00:09:32 -0400

    style(ui): updates message view. adds automatic expansion to message input.

--- src/views/Messages.jsx
+++ src/views/Messages.jsx
@@ -1,20 +1,29 @@
 /* eslint-disable */
 const React = require('react');
 const { Component } = require('react');
-const { Avatar, Button, Checkbox, FontIcon, IconButton,
+const { Avatar, FontIcon, IconButton,
         Input, List, ListItem, ListSubHeader, Panel } = require('react-toolbox');
 
 class Messages extends Component {
+    state = { multiline: '' };
+
+
+    handleChange = (name, value) => {
+        this.setState({ ...this.state, [name]: value });
+    };
+
+
     render() {
         return (
             <Panel>
                 <div className="messages">
                     <div className="message-list">
                         <List selectable ripple>
-                            <ListSubHeader caption="Direct messages"/>
-                            <ListItem caption="Alice" className="online active" leftIcon="fiber_manual_record"/>
+                            <ListSubHeader caption="Direct messages" />
+                            <ListItem caption="Alice" className="online active" leftIcon="fiber_manual_record" />
                             <ListItem caption="Albert" leftIcon="fiber_manual_record" />
-                            <ListItem caption="Bill" className="online" leftIcon="fiber_manual_record" rightIcon={<div className="notification">12</div>} />
+                            <ListItem caption="Bill" className="online" leftIcon="fiber_manual_record"
+                                rightIcon={<div className="notification">12</div>} />
                             <ListItem caption="Jeff" className="online" leftIcon="fiber_manual_record" />
                             <ListItem caption="Steve" className="busy" leftIcon="fiber_manual_record" />
                         </List>
@@ -24,33 +33,35 @@ class Messages extends Component {
                             <div className="title">Alice</div>
                             {/* TODO create Search component */}
                             <div className="search">
-                                <FontIcon value="search" className="search-icon"/>
-                                <Input placeholder="search"/>
+                                <FontIcon value="search" className="search-icon" />
+                                <Input placeholder="search" />
                                 {/* FIXME: Should only be visible when input has focus */}
-                                <IconButton icon="highlight_off"/>
+                                <IconButton icon="highlight_off" />
                             </div>
-                            <IconButton icon="info_outline"/>
+                            <IconButton icon="info_outline" />
                         </div>
                         <div className="message">
-                            <Avatar>
-                                <img src="https://placeimg.com/80/80/animals" alt="avatar"/>
-                            </Avatar>
-                            <div className="message-content">
-                                <div className="meta-data">
-                                    <div className="user">Bob</div>
-                                    <div className="timestamp">8:29 AM</div>
+                            <div className="message-content-wrapper">
+                                <Avatar>
+                                    <img src="https://placeimg.com/80/80/animals" alt="avatar" />
+                                </Avatar>
+                                <div className="message-content">
+                                    <div className="meta-data">
+                                        <div className="user">Bob</div>
+                                        <div className="timestamp">8:29 AM</div>
+                                    </div>
+                                    <p>Message content</p>
                                 </div>
-                                <p>Message content</p>
                             </div>
-
                         </div>
                         <div className="message-input">
-                            <IconButton icon="add_circle_outline"/>
-                            <Input multiline type="text" placeholder="Messages Alice"/>
+                            <IconButton icon="add_circle_outline" />
+                            <Input multiline type="text" placeholder="Messages Alice"
+                                onChange={this.handleChange.bind(this, 'multiline')} />
                             {/* <Checkbox
                                 checked
                                 label="Send on enter" /> */}
-                            <IconButton icon="thumb_up"/>
+                            <IconButton icon="thumb_up" />
                             {/*
                                 depending on how users react to no send button
                                 maybe flip between this and thumbs_up?

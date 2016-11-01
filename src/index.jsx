21cd78545cd3040e53c332c8b6ca7cffbb5c3026
flohdot <floh@flohdot.org>
2016-11-01 17:28:14 -0400

    fix(tinydb): use icebear implementation

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,19 @@
 /* eslint-disable */
 window.location.hash = '';
 document.addEventListener('DOMContentLoaded', () => {
-    
+
     require.extensions['.css'] = function (m, f) {
         m.exports = require(f.replace('.css', '.json'));
     };
-    
+
     const React = require('react');
-    const {socket, config} = require('./icebear');
+    const {socket, config, setTinyDbEngine } = require('./icebear');
     const {render} = require('react-dom');
     const {Router, hashHistory} = require('react-router');
     const routes = require('./routes');
+    const tinyStorage = require('./stores/tiny-db');
+
+    setTinyDbEngine(tinyStorage);
 
     config.socketServerUrl = 'wss://hocuspocus.peerio.com';
     socket.start();

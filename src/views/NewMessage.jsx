e3e5a3507a063e6047f1cf73d7cbfae7a70df68f
paulrdukes <paul.r.dukes@gmail.com>
2016-11-03 15:08:37 -0400

    style(UI): make new message creation screen even more beautiful than before.

--- src/views/NewMessage.jsx
+++ src/views/NewMessage.jsx
@@ -1,18 +1,23 @@
 const React = require('react');
 const { observable } = require('mobx');
 const { observer } = require('mobx-react');
-const { Button, Chip, Input, List, ListItem } = require('react-toolbox');
+const { Button, Chip, Input, List, ListItem, ListSubHeader } = require('react-toolbox');
 
 @observer
 class NewMessage extends React.Component {
     render() {
         return (
-            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100vw', height: '100vh' }}>
+            <div className="new-message" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100vw', height: '100vh' }}>
                 <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '600px', marginTop: '168px' }} >
                     <div className="new-message-search"><Chip deletable>user name</Chip>
                         <Input /> <Button className="confirm" label="Go" />
                     </div>
-                    <List>
+                    <List selectable ripple >
+                        <ListSubHeader caption="Your contacts" />
+                        <ListItem
+                            avatar="https://placeimg.com/80/80/animals"
+                            caption="User name"
+                            legend="some text about the user" />
                         <ListItem
                             avatar="https://placeimg.com/80/80/animals"
                             caption="User name"

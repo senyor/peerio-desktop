a3e45c9dbc968e6e08964df1f63b7cf8783e3f65
paulrdukes <paul.r.dukes@gmail.com>
2016-11-03 14:32:39 -0400

    style(ui): updates new message creation screen markup.

--- src/views/NewMessage.jsx
+++ src/views/NewMessage.jsx
@@ -1,8 +1,7 @@
 const React = require('react');
 const { observable } = require('mobx');
 const { observer } = require('mobx-react');
-const { Button, List, ListItem } = require('react-toolbox');
-const Search = require('../components/Search');
+const { Button, Chip, Input, List, ListItem } = require('react-toolbox');
 
 @observer
 class NewMessage extends React.Component {
@@ -10,8 +9,8 @@ class NewMessage extends React.Component {
         return (
             <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100vw', height: '100vh' }}>
                 <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '600px', marginTop: '168px' }} >
-                    <div className="new-message-search">
-                        <Search /> <Button className="confirm" label="Go!" />
+                    <div className="new-message-search"><Chip deletable>user name</Chip>
+                        <Input /> <Button className="confirm" label="Go" />
                     </div>
                     <List>
                         <ListItem

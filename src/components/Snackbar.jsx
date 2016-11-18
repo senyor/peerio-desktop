ac6a55568df66646776e496ed5d743187bd15765
paulrdukes <paul.r.dukes@gmail.com>
2016-11-18 11:53:52 -0500

    WIP: snackbar

---
+++ src/components/Snackbar.jsx
@@ -0,0 +1,23 @@
+const React = require('react');
+const { Button } = require('react-toolbox');
+const { observer } = require('mobx-react');
+const css = require('classNames');
+
+
+@observer
+class Snackbar extends React.Component {
+
+    render() {
+        return (
+            <div className={css(this.props.className)}>
+                {this.props.content}
+                {/* TODO make optional */}
+                {this.props.label && this.props.action !== null ?
+                    <Button label={this.props.label} onClick={this.props.action} /> : null
+                }
+            </div>
+        );
+    }
+}
+
+module.exports = Snackbar;

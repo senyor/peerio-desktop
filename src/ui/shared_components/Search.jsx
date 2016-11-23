3df030cf34602c4ffa5d3c19dd68ee3558ba3282
Eren Bilgin <eren@peerio.com>
2016-11-23 12:53:15 -0500

    chore(i18n): localizing placeholder text

--- src/ui/shared_components/Search.jsx
+++ src/ui/shared_components/Search.jsx
@@ -1,12 +1,13 @@
 const React = require('react');
 const { FontIcon, IconButton, Input } = require('react-toolbox');
+const { t } = require('peerio-translator');
 
 class Search extends React.Component {
     render() {
         return (
             <div className="search">
                 <FontIcon value="search" className="search-icon" />
-                <Input placeholder="search" />
+                <Input placeholder={t('messageSearch')} />
                 {/* FIXME: Should only be visible when input has focus */}
                 <IconButton icon="highlight_off" />
             </div>

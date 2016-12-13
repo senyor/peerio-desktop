aa445294b1e9fa123919d5f2123b04dbbf165c20
Anri Asaturov <anri82@gmail.com>
2016-12-13 03:53:24 +0200

    feat(chats): JUMBOJI

--- src/ui/messages/components/Message.jsx
+++ src/ui/messages/components/Message.jsx
@@ -2,23 +2,42 @@ const React = require('react');
 const Avatar = require('../../shared-components/Avatar');
 const { observer } = require('mobx-react');
 const { time } = require('../../../helpers/formatter');
-const { FontIcon } = require('react-toolbox');
 const Interweave = require('interweave').default;
+const css = require('classnames');
 
-const Message = observer(({ message }) => {
-    return (
-        <div className="message-content-wrapper">
-            <Avatar contact={message.sender} />
-            <div className="message-content">
-                <div className="meta-data">
-                    <div className="user">{message.sender.username}</div>
-                    <div className="timestamp">{time.format(message.timestamp)}</div>
+@observer
+class Message extends React.Component {
+    isJumboji = false;
+
+    afterParse = (arr) => {
+        if (this.isJumboji) return arr;
+        if (arr.length > 10) return arr;
+        for (let i = 0; i < arr.length; i++) {
+            if (!arr[i].type || arr[0].type.name !== 'Emoji') {
+                return arr;
+            }
+        }
+        this.isJumboji = true;
+        setTimeout(this.forceUpdate.bind(this));
+        return arr;
+    };
+
+    render() {
+        return (
+            <div className={css('message-content-wrapper', { jumboji: this.isJumboji })}>
+                <Avatar contact={this.props.message.sender} />
+                <div className="message-content">
+                    <div className="meta-data">
+                        <div className="user">{this.props.message.sender.username}</div>
+                        <div className="timestamp">{time.format(this.props.message.timestamp)}</div>
+                    </div>
+                    <Interweave tagName="p" className="test" content={this.props.message.text} noHtml
+                                onAfterParse={this.afterParse} />
                 </div>
-                <Interweave tagName="p" content={message.text} noHtml />
+                {this.props.message.sending ? <div className="sending-overlay" /> : null}
             </div>
-            {message.sending ? <div className="sending-overlay" /> : null}
-        </div>
-    );
-});
+        );
+    }
+}
 
 module.exports = Message;

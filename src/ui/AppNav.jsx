8b9304fa0f85eab938fd23c05d4388da9446433b
Anri Asaturov <anri82@gmail.com>
2017-01-07 16:56:40 +0200

    feat(security): passphrase display

--- src/ui/AppNav.jsx
+++ src/ui/AppNav.jsx
@@ -1,13 +1,13 @@
 const React = require('react');
 const { observable, autorunAsync } = require('mobx');
 const { observer } = require('mobx-react');
-const { IconButton, Tooltip } = require('~/react-toolbox');
+const { IconButton, Tooltip, IconMenu, MenuItem, MenuDivider, Dialog } = require('~/react-toolbox');
 const { User, contactStore, chatStore } = require('~/icebear');
 const Avatar = require('~/ui/shared-components/Avatar');
 const css = require('classnames');
 const app = require('electron').remote.app;
 const sounds = require('~/helpers/sounds');
-const signout = require('~/helpers/app-control');
+const appControl = require('~/helpers/app-control');
 
 const TooltipIcon = Tooltip(IconButton); //eslint-disable-line
 
@@ -18,8 +18,12 @@ const delay = 500;
 class AppNav extends React.Component {
 
     @observable inMessages = true;
+    @observable passphraseVisible = false;
 
-
+    hidePassphrase = () => { this.passphraseVisible = false; };
+    passphraseDialogActions = [
+        { label: 'Ok', onClick: this.hidePassphrase }
+    ];
     componentWillMount() {
         this.contact = contactStore.getContact(User.current.username);
         autorunAsync(() => {
@@ -34,6 +38,7 @@ class AppNav extends React.Component {
         });
     }
 
+    // todo: terrible idea(inMessages), do this based on router hooks
     toMessages = () => {
         window.router.push('/app');
         this.inMessages = true;
@@ -48,19 +53,25 @@ class AppNav extends React.Component {
         return false;
     };
 
-    toSignout = () => {
-        signout.relaunch();
-    }
+    signout = () => {
+        appControl.relaunch();
+    };
+
+    showPassphrase= () => {
+        this.passphraseVisible = true;
+    };
+
     render() {
         return (
             <div className="app-nav">
                 <div className="avatar-wrapper">
-                    {/* <IconMenu icon="">
-                        <MenuItem value="profile" icon="person" caption="Profile" disabled />
-                        <MenuItem value="Settings" icon="settings" caption="Settings" disabled />
+                    <IconMenu icon="">
+                        <MenuItem value="passphrase" icon="lock" caption="Show passphrase"
+                                  onClick={this.showPassphrase} />
                         <MenuDivider />
-                        <MenuItem value="signout" icon="power_settings_new" caption="Sign out" onClick={this.toSignout} />
-                    </IconMenu> */}
+                        <MenuItem value="signout" icon="power_settings_new" caption="Sign out"
+                                  onClick={this.signout} />
+                    </IconMenu>
                     <Avatar contact={this.contact} />
                 </div>
                 <div className="app-menu">
@@ -85,6 +96,10 @@ class AppNav extends React.Component {
                         <div className={this.unreadFiles ? '' : ''} />
                     </div>
                 </div>
+                <Dialog active={this.passphraseVisible} actions={this.passphraseDialogActions}
+                        onOverlayClick={this.hidePassphrase} onEscKeyDown={this.hidePassphrase}>
+                    <div className="passphrase-display">{User.current.passphrase}</div>
+                </Dialog>
             </div>
         );
     }

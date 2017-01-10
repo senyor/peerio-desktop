3ab8f12897c2396cb891889eb7d563bb76a4a49b
Anri Asaturov <anri82@gmail.com>
2017-01-10 02:14:37 +0200

    refactor(config): everything goes through config object

--- src/config.js
+++ src/config.js
@@ -1,20 +1,29 @@
 const cfg = require('~/icebear').config;
 const app = require('electron').app || require('electron').remote.app;
 const isDevEnv = require('~/helpers/is-dev-env');
+const tinyDb = require('~/stores/tiny-db');
+const FileStream = require('~/helpers/file-stream');
 
 cfg.appName = 'Icebear';
 cfg.updateUrl = 'https://avadakedavra.peerio.com/update';
 cfg.currentVersion = app.getVersion();
 cfg.socketServerUrl = 'wss://hocuspocus.peerio.com';
 
+cfg.TinyDb = {
+    getValue: tinyDb.getValueAsync,
+    setValue: tinyDb.setValueAsync,
+    removeValue: tinyDb.removeAsync
+};
 
-cfg.stringReplacements = [];
+cfg.FileStream = FileStream;
+
+cfg.stringReplacements = []; // white label only
 
 if (isDevEnv) {
     try {
         cfg.autologin = require('../../autologin.json'); // eslint-disable-line
     } catch (err) {
-    // don't care
+        // don't care
     }
 }
 

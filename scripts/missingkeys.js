12505ed47ba0e4f683597ba685906ab9b1cfae7c
Eren Bilgin <eren@peerio.com>
2017-01-13 11:01:41 -0500

    chore(i18n): script to find missing keys

---
+++ scripts/missingkeys.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const glob = require('glob');
+
+const keys = JSON.parse(fs.readFileSync('../src/static/locales/en.json'));
+
+
+glob('../**/*.jsx', (err, files) => {
+    for (const i of files) {
+        const file = fs.readFileSync(i, 'utf8');
+        const re = /[^r]t\('(.*?)'/gm;
+        let matches = re.exec(file);
+        console.log(i);
+        while (matches != null) {
+            if (typeof keys[matches[1]] === 'undefined') {
+                console.log(matches[1]);
+            }
+            matches = re.exec(file);
+        }
+    }
+});
+ 

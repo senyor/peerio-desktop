60239ec3e5cb5c19e39cb7293881b04ae5450db6
paulrdukes <paul.r.dukes@gmail.com>
2017-01-17 13:36:31 -0500

    style: adds keys to preferences.

--- src/ui/settings/components/Preferences.jsx
+++ src/ui/settings/components/Preferences.jsx
@@ -2,6 +2,7 @@ const React = require('react');
 const { observable } = require('mobx');
 const { observer } = require('mobx-react');
 const { Switch } = require('~/react-toolbox');
+const { t } = require('peerio-translator');
 
 @observer class Preferences extends React.Component {
 
@@ -9,21 +10,27 @@ const { Switch } = require('~/react-toolbox');
         return (
             <div>
                 <section className="section-divider">
-                    <div className="title">Notifications</div>
-                    <p>Email you when ...</p>
-                    <Switch checked="true" label="Never" />
-                    <Switch checked="false" label="You receive a new message" />
-                    <Switch checked="false" label="You receive a new file" />
-                    <Switch checked="false" label="You receive a contact request" />
+                    <div className="title">{t('notifications')}</div>
+                    <p>
+                        {t('descirption_notification')}
+                        {/* Email you when... */}
+                    </p>
+                    <Switch checked="true" label={t('never')} />
+                    <Switch checked="false" label={t('newMessage')} />
+                    <Switch checked="false" label={t('newFile')} />
+                    <Switch checked="false" label={t('contactRequest')} />
                 </section>
 
                 <section>
-                    <div className="title">Privacy</div>
-                    <p>Other users can find you ...</p>
-                    <Switch checked="true" label="Never" />
-                    <Switch checked="false" label="By name" />
-                    <Switch checked="false" label="By username" />
-                    <Switch checked="false" label="By email" />
+                    <div className="title">{t('privacy')}</div>
+                    <p>
+                        {t('descirption_privacy')}
+                        {/* Other users can find you... */}
+                    </p>
+                    <Switch checked="true" label={t('never')} />
+                    <Switch checked="false" label={t('byName')} />
+                    <Switch checked="false" label={t('byUsername')} />
+                    <Switch checked="false" label={t('byEmail')} />
                 </section>
             </div>
         );

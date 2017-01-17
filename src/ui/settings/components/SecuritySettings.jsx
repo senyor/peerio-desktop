622c14bada0ddb53b466df7ed3ab7f96e0d0683c
paulrdukes <paul.r.dukes@gmail.com>
2017-01-17 13:25:23 -0500

    style: adds security settings keys.

--- src/ui/settings/components/SecuritySettings.jsx
+++ src/ui/settings/components/SecuritySettings.jsx
@@ -3,6 +3,7 @@ const { observable } = require('mobx');
 const { observer } = require('mobx-react');
 const { Button, Dialog } = require('~/react-toolbox');
 const { User } = require('~/icebear');
+const { t } = require('peerio-translator');
 
 @observer class SecuritySettings extends React.Component {
 
@@ -21,37 +22,58 @@ const { User } = require('~/icebear');
         return (
             <div>
                 <section className="section-divider">
-                    <div className="title" >Device Password
-                        <Button label="update" flat primary />
+                    <div className="title" >
+                        {t('peerioPINForThisDevicedesktop')}
+                        <Button label={t('button_update')} flat primary />
                     </div>
                     <p style={{ marginBottom: '40px' }}>
-                        Your device password is used to log in to this device only.
+                        {t('desciption_DevicePassword')}
+                        {/*
+                          Your device password is only used to log in to this device.
+                        */}
                     </p>
-                    <div className="title">Two factor authentication (2FA)
-                        <Button label="activate" flat primary />
+                    <div className="title">{t('2fa')}
+                        <Button label={t('button_activate')} flat primary />
                     </div>
-                    <p>An additional layer of account protection.</p>
+                    <p>
+                        {t('desciption_2fa')}
+                        {/* An additional layer of account protection. */}
+                    </p>
                 </section>
                 <section className="section-divider">
-                    <div className="title">QR Login
-                        <Button label="generate" flat primary />
+                    <div className="title">{t('QRLogin')}
+                        <Button label={t('button_generate')} flat primary />
                     </div>
-                    <p style={{ marginBottom: '40px' }}>Generate a
-                    single use QR code to log in to other devices.</p>
+                    <p style={{ marginBottom: '40px' }}>
+                        {t('desciption_QRLogin')}
+                        {/*
+                          Generate a single use QR code to log in to other devices.
+                        */}
+                    </p>
 
-                    <div className="title">Master password
-                        <Button label="view"
+                    <div className="title">{t('passphrase')}
+                        <Button label={t('button_view')}
                                 onClick={this.showPassphrase} flat primary />
                     </div>
-                    <p>Your Master Password is required when logging
-                    to a new device and after a clean install.</p>
+                    <p>
+                        {t('desciption_MasterPassword')}
+                        {/*
+                          Your Master Password is required when logging
+                          to a new device and after a clean install.
+                        */}
+                    </p>
                 </section>
                 <section>
-                    <div className="title">Access logs
-                        <Button label="view" flat primary />
+                    <div className="title"> {t('accessLogs')}
+                        <Button label={t('button_view')} flat primary />
                     </div>
-                    <p>Audit access to your account.<br />
-                    3 devices have accessed your account in the last 30 days.</p>
+                    <p>
+                        {t('desciption_accessLogs')}
+                        {/*
+                          Audit access to your account.<br />
+                          3 devices have accessed your account in the last 30 days.
+                        */}
+                    </p>
                 </section>
 
                 <Dialog active={this.passphraseVisible} actions={this.passphraseDialogActions}

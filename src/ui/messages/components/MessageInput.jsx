c6daffc1d32990fb19f6f82e0bee06f4e63aa250
Anri Asaturov <anri82@gmail.com>
2017-01-26 08:56:18 +0200

    feat(files): disable sharing of incomplete files

--- src/ui/messages/components/MessageInput.jsx
+++ src/ui/messages/components/MessageInput.jsx
@@ -9,6 +9,7 @@ const emojione = require('~/static/emoji/emojione.js');
 const Quill = require('quill/dist/quill.core');
 const { sanitizeChatMessage } = require('~/helpers/sanitizer');
 const FilePicker = require('~/ui/files/components/FilePicker');
+const { fileStore } = require('~/icebear');
 
 // todo: this file is messy as hell, maybe refactor it
 
@@ -177,6 +178,7 @@ class MessageInput extends React.Component {
     };
 
     showFilePicker = () => {
+        fileStore.clearSelection();
         this.filePickerActive = true;
     };
 

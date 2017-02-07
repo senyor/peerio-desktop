e1f6c992510ba9a80d84eca959ba27fc3a06dd35
Anri Asaturov <anri82@gmail.com>
2017-02-07 17:23:56 +0200

    feat(security): passckde lock component

---
+++ src/ui/shared-components/PasscodeLock.jsx
@@ -0,0 +1,47 @@
+const React = require('react');
+const { observable } = require('mobx');
+const { observer } = require('mobx-react');
+const { Input, ProgressBar } = require('~/react-toolbox');
+const { t } = require('peerio-translator');
+const { User } = require('~/icebear');
+const _ = require('lodash');
+
+@observer
+class PasscodeLock extends React.Component {
+    @observable passcode;
+    @observable error;
+
+    handleChange = (val) => {
+        this.passcode = val;
+        this.validatePasscode();
+    };
+
+    unlock = () => {
+        this.props.onUnlocked();
+        this.error = null;
+    };
+
+    handleError =() => {
+        this.error = t('invalidPasscode');
+    };
+
+    validatePasscode = _.debounce(() => {
+        User.current.validatePasscode(this.passcode)
+            .then(this.unlock)
+            .catch(this.handleError);
+    }, 350);
+
+    render() {
+        return (
+            <div className="passcode-lock">
+                <Input type="password"
+                    value={this.passcode}
+                       label={t('unlockWithPasscode')}
+                       onChange={this.handleChange}
+                       error={this.error} />
+            </div>
+        );
+    }
+}
+
+module.exports = PasscodeLock;

4c868566036e10635ae3e1ce91ab72ab7e77a44c
Anri Asaturov <anri82@gmail.com>
2017-03-06 07:14:22 +0200

    fix(security): prevent popups and filter protocols

--- src/main-process/misc-hooks.js
+++ src/main-process/misc-hooks.js
@@ -2,11 +2,29 @@ const { shell } = require('electron');
 
 function applyMiscHooks(mainWindow) {
     console.log('Attaching misc webContents hooks.');
+
+    const allowedProtocols = ['HTTP://', 'HTTPS://', 'MAILTO://'];
+    function isUrlAllowed(url) {
+        if (typeof url !== 'string') return false;
+        const URL = url.toUpperCase(url).trim();
+        for (let i = 0; i < allowedProtocols.length; i++) {
+            if (URL.startsWith(allowedProtocols[i])) return true;
+        }
+        return false;
+    }
+
     // make all attempts to navigate browser window open in external browser
     mainWindow.webContents.on('will-navigate', (e, url) => {
         if (url !== mainWindow.webContents.getURL()) {
             e.preventDefault();
-            shell.openExternal(url);
+            if (isUrlAllowed(url)) shell.openExternal(url);
+        }
+    });
+
+    mainWindow.webContents.on('new-window', (e, url) => {
+        if (url !== mainWindow.webContents.getURL()) {
+            e.preventDefault();
+            if (isUrlAllowed(url)) shell.openExternal(url);
         }
     });
 }

ea3d96b81c5133a5c7ff3cb447d2c3ade4e48a46
Anri Asaturov <anri82@gmail.com>
2017-03-08 08:36:21 +0200

    WIP: chats

--- src/ui/messages/components/Message.jsx
+++ src/ui/messages/components/Message.jsx
@@ -49,7 +49,7 @@ function processMessage(msg) {
 class Message extends React.Component {
     render() {
         const m = this.props.message;
-        console.log('Rendering message ', m.tempId || m.id);
+       // console.log('Rendering message ', m.tempId || m.id);
         const invalidSign = m.signatureError === true;
 
         return (
--- src/ui/messages/components/MessageList.jsx
+++ src/ui/messages/components/MessageList.jsx
@@ -1,32 +1,87 @@
 const React = require('react');
+const { reaction } = require('mobx');
 const { observer } = require('mobx-react');
 const { ProgressBar } = require('~/react-toolbox');
 const Avatar = require('~/ui/shared-components/Avatar');
 const Message = require('./Message');
 const { chatStore } = require('~/icebear');
 const { t } = require('peerio-translator');
+const _ = require('lodash');
 
 @observer
 class MessageList extends React.Component {
 
-    componentDidMount() {
-        this.scrollToBottom();
+    loadTriggerDistance = 20;
+    stickDistance = 220;
+
+    componentWillMount() {
+        this.stickToBottom = true;
+        this._reaction = reaction(() => chatStore.activeChat && chatStore.activeChat.loadingTopPage, (loading) => {
+            if (loading) {
+                this.lastTopElement = this.containerRef.childNodes[1];
+                this.lastTopElementOffset = this.lastTopElement.offsetTop;
+                return;
+            }
+            if (this.lastTopElement) {
+                setTimeout(() => {
+                    // todo: animate
+                    this.containerRef.scrollTop = this.lastTopElement.offsetTop - this.lastTopElementOffset - 100;
+                    this.lastTopElement = null;
+                }, 200);
+            }
+        });
+        this._resetReaction = reaction(() => chatStore.activeChat.loadingInitialPage, () => {
+            this.stickToBottom = true;
+        });
+    }
+    componentWillUnmount() {
+        this._reaction();
+        this._resetReaction();
     }
 
     componentDidUpdate() {
-        this.scrollToBottom();
+        if (this.stickToBottom) {
+            setTimeout(this.scrollToBottom, 100);
+        }
     }
 
-    scrollToBottom() {
-        const el = document.getElementsByClassName('messages-container')[0];
-        if (!el) return;
-        setTimeout(() => {
-            el.scrollTop = el.scrollHeight;
-        }, 100);
-    }
+    scrollToBottom = () => {
+        if (!this.containerRef) return;
+        this.containerRef.scrollTop = this.containerRef.scrollHeight - this.containerRef.clientHeight - 20;
+    };
+
+    handleScroll = _.debounce(() => {
+        // we can't handle scroll if content height is too small
+        if (this.containerRef.scrollHeight <= this.containerRef.clientHeight) return;
+
+        const distanceToBottom = this.containerRef.scrollHeight - this.containerRef.scrollTop
+                                    - this.containerRef.clientHeight;
+        const distanceToTop = this.containerRef.scrollTop;
+        // detecting sticking state
+        this.stickToBottom = distanceToBottom < this.stickDistance && !chatStore.activeChat.canGoDown;
+        // triggering page load
+        if (distanceToBottom < this.loadTriggerDistance) {
+            chatStore.activeChat.loadNextPage();
+        }
+        if (distanceToTop < this.loadTriggerDistance) {
+            chatStore.activeChat.loadPreviousPage();
+        }
+    }, 250);
+
+    setContainerRef = (r) => {
+        this.containerRef = r;
+    };
+
 
     renderMessages() {
         const ret = [];
+        if (chatStore.activeChat.canGoUp) {
+            ret.push(<div key="top-progress-bar" className="message-content-wrapper"
+                      style={{ visibility: chatStore.activeChat.loadingTopPage ? 'visible' : 'hidden' }}>
+                <ProgressBar type="circular" mode="indeterminate" multicolor
+                                  className="messages-inline-progress-bar" />
+            </div>);
+        }
         const msgs = chatStore.activeChat.messages;
         for (let i = 0; i < msgs.length; i++) {
             const m = msgs[i];
@@ -49,28 +104,47 @@ class MessageList extends React.Component {
             }
             ret.push(<Message key={m.tempId || m.id} message={m} light={light} />);
         }
+
+        if (chatStore.activeChat.canGoDown) {
+            ret.push(<div key="bot-progress-bar" className="message-content-wrapper"
+                style={{ visibility: chatStore.activeChat.loadingBottomPage ? 'visible' : 'hidden' }}>
+                <ProgressBar type="circular" mode="indeterminate" multicolor
+                                  className="messages-inline-progress-bar" />
+            </div>);
+        }
+
         return ret;
     }
 
     render() {
         if (!chatStore.activeChat) return null;
+        const style = {
+            paddingTop: `${this.loadTriggerHeight}px`,
+            paddingBottom: `${this.loadTriggerHeight}px`
+        };
         return (
-            <div className="messages-container">
+            <div className="messages-container" style={style}
+                    onScroll={this.handleScroll} ref={this.setContainerRef}>
+                { chatStore.activeChat.canGoUp || !chatStore.activeChat.initialPageLoaded ? null :
                 <div className="messages-start">
                     <div className="avatars">
                         {chatStore.activeChat.participants.map(c =>
                             <Avatar key={c.username} contact={c} />)}
                     </div>
                     <div className="title">
-                        This is the beginning of your chat history with
-                        &nbsp;<strong>{chatStore.activeChat.chatName}</strong>.
-                    </div>
+                            This is the beginning of your chat history with
+                            &nbsp;<strong>{chatStore.activeChat.chatName}</strong>.
+                        </div>
                 </div>
-                {chatStore.activeChat.loadingPage
+                }
+
+                {chatStore.activeChat.loadingInitialPage
                     ? <ProgressBar type="circular" mode="indeterminate"
                                    multicolor className="messages-progress-bar" />
                     : this.renderMessages()
                 }
+                {chatStore.activeChat.loadiingBottomPage ? <ProgressBar type="circular" mode="indeterminate"
+                                   multicolor className="messages-progress-bar bottom" /> : null }
             </div>
         );
     }

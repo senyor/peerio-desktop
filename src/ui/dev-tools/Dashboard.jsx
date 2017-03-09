a41d19577ede510a6d520a1a12dd35a87e6e9d0a
Anri Asaturov <anri82@gmail.com>
2017-03-09 19:35:19 +0200

    feat(devtool): server change tool

--- src/ui/dev-tools/Dashboard.jsx
+++ src/ui/dev-tools/Dashboard.jsx
@@ -1,19 +1,27 @@
 const React = require('react');
-const { Card, CardTitle, CardText } = require('~/react-toolbox');
-const { User } = require('~/icebear');
+const { Card, CardTitle, CardText, Input, Button } = require('~/react-toolbox');
+const { User, socket } = require('~/icebear');
 const config = require('~/config');
+const { observable } = require('mobx');
+const { observer } = require('mobx-react');
 
 class DevToolsDashboard extends React.Component {
     render() {
         return (
             <div className="dashboard">
-                <Card style={{ width: '400px' }}>
+                <Card style={{ width: '100%' }}>
+                    <CardTitle title="Socket server" />
+                    <CardText>
+                        <ChangeServer />
+                    </CardText>
+                </Card>
+                <Card style={{ width: '35%' }}>
                     <CardTitle title="Current user" />
                     <CardText>
                         <UserDetails />
                     </CardText>
                 </Card>
-                <Card>
+                <Card style={{ width: '57%' }}>
                     <CardTitle title="Config" />
                     <CardText>
                         <pre>{JSON.stringify(config, null, 2)}</pre>
@@ -22,7 +30,6 @@ class DevToolsDashboard extends React.Component {
             </div>
         );
     }
-
 }
 
 function UserDetails() {
@@ -36,6 +43,34 @@ function UserDetails() {
             Last name: <strong>{User.current.lastName}</strong>
         </span>
     );
+}
+
+@observer
+class ChangeServer extends React.Component {
+    @observable server;
+    onServerChange = v => {
+        this.server = v;
+    };
+    changeServer = () => {
+        socket.close();
+        socket.socket.io.uri = this.server;
+        socket.open();
+        this.forceUpdate();
+    };
+    render() {
+        if (User.current) {
+            return <span>Can't change server after login, sign out first.</span>;
+        }
+
+        return (
+            <span>
+                Current server: {socket.socket.io.uri}
+                <Input type="text" value={this.server} onChange={this.onServerChange}
+                       label="Server url" /><br />
+                <Button label="Change and reconnect" raised primary onClick={this.changeServer} />
+            </span>
+        );
+    }
 }
 
 

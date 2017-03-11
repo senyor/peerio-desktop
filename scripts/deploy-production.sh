629eb8fb3dd837f5ed05c3c7bfddaddd6a083db1
Anri Asaturov <anri82@gmail.com>
2017-03-11 17:36:53 +0200

    chore(project): standard-version clone

--- scripts/deploy-production.sh
+++ scripts/deploy-production.sh
@@ -8,7 +8,7 @@ rm -rf dist
 echo "[••••••      ] compiling sources"
 npm run compile
 echo "[••••••••    ] creating github release"
-./node_modules/.bin/peerio-desktop-standard-version
+./node_modules/.bin/standard-version
 git push --follow-tags origin master
 # echo "[••••••••••  ] building 32-bit bundles"
 # build --windows --linux --ia32 --publish always

85cf92559926d2bcfda0a678cd9875abf7bf69f0
Anri Asaturov <anri82@gmail.com>
2017-03-16 17:05:39 +0200

    chore(locale): switch to icebear locale file

--- scripts/get-locales.sh
+++ scripts/get-locales.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-cp node_modules/peerio-copy/client_en.json src/static/locales/en.json
+cp node_modules/peerio-copy/icebear_en.json src/static/locales/en.json
 cp -R node_modules/peerio-copy/phrase/dict src/static/locales
 tx pull -af

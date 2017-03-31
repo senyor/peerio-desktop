6cbaf6b8e0ae583638a706896114ade47d31b031
Anri Asaturov <anri82@gmail.com>
2017-03-31 18:07:46 +0300

    chore(config): distribution signing support

--- scripts/deploy-production.sh
+++ scripts/deploy-production.sh
@@ -3,6 +3,9 @@
 echo "[•••            ] setting flags"
 set -e
 export NODE_ENV=production
+export CSC_LINK=~/.cert/peerio_mac.p12
+# EV certs use token instead of file, subject is set in electron-builder config: win/certificateSubjectName
+# export WIN_CSC_LINK=~/.cert
 echo "[••••••         ] cleaning up"
 rm -rf dist
 echo "[•••••••••      ] compiling sources"
@@ -11,4 +14,4 @@ echo "[••••••••••••   ] creating github release"
 ./node_modules/.bin/standard-version
 git push --follow-tags origin master
 echo "[•••••••••••••••] building bundles"
-build --mac --windows --linux  --ia32 --x64 --publish always
+build --mac --windows --ia32 --x64 --publish always

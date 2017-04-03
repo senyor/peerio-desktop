03fb885ad2d0bf43e09d86fa56e6ac76b6298b99
Dmitry Chestnykh <dmitry@codingrobots.com>
2017-04-04 01:27:30 +0200

    fix(drag): allow only file drop, use 'copy' cursor
    Dragging text fragments caused the drop target overlay to appear.
    Now it only appears for file(s).
    
    It was also possible for a user to change mouse cursor appearance
    depending on dragging operation (by pressing Shift, Alt, etc.).
    Now only 'copy' cursor will appear.

--- src/stores/drag-drop-store.js
+++ src/stores/drag-drop-store.js
@@ -9,7 +9,7 @@ class DragDropStore {
 
     _onEnter = (ev) => {
         ev.preventDefault();
-        if (!User.current) return;
+        if (!User.current || !ev.dataTransfer.files.length) return;
         this._counter++;
         if (this._counter === 1) {
             this.hovering = true;
@@ -19,7 +19,7 @@ class DragDropStore {
 
     _onLeave = (ev) => {
         ev.preventDefault();
-        if (!User.current) return;
+        if (!User.current || !ev.dataTransfer.files.length) return;
         if (this._counter > 0) this._counter--;
         if (this._counter === 0) this.hovering = false;
     };
@@ -39,6 +39,17 @@ class DragDropStore {
         }
     };
 
+    _onOver = (ev) => {
+        ev.preventDefault();
+        if (!User.current || !ev.dataTransfer.files.length) {
+            ev.dataTransfer.dropEffect = 'none';
+            ev.dataTransfer.effectAllowed = 'none';
+        } else {
+            ev.dataTransfer.dropEffect = 'copy';
+            ev.dataTransfer.effectAllowed = 'copy';
+        }
+    }
+
     /**
      * Subscribe handler to files dropped event
      * @param {Function<Array<string>>} handler
@@ -52,6 +63,6 @@ const store = new DragDropStore();
 window.addEventListener('drop', store._onDrop, false);
 window.addEventListener('dragenter', store._onEnter, false);
 window.addEventListener('dragleave', store._onLeave, false);
-window.addEventListener('dragover', (ev) => { ev.preventDefault(); }, false);
+window.addEventListener('dragover', store._onOver, false);
 
 module.exports = store;

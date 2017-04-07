7d16f454136b1c56ee6e4da8d6e8f4eb87cf78a3
Paul Dukes <paul.r.dukes@gmail.com>
2017-04-07 13:12:28 -0400

    style: updates zero state layout.

--- src/ui/messages/components/NoChatSelected.jsx
+++ src/ui/messages/components/NoChatSelected.jsx
@@ -3,26 +3,35 @@ const { t } = require('peerio-translator');
 
 function NoChatSelected() {
     return (
-        <div className="flex-row zero-message">
-            <div className="flex-col flex-grow-0" style={{ margin: '16px 24px' }}>
-                <img src="static/img/message-arrow-up.png" style={{ maxWidth: '200px' }} alt="" />
-            </div>
+        <div className="zero-message" style={{ postion: 'relative' }}>
+            <img src="static/img/message-arrow-up.png"
+                 style={{
+                     maxWidth: '200px',
+                     position: 'absolute',
+                     left: '24px',
+                     top: '80px'
+                 }} alt="" />
             <div className="flex-col flex-grow-0 flex-shrink-0">
                 <div className="flex-row flex-justify-center">
                     <div className="display-3">{t('title_chatNull1')}</div>
                 </div>
-                <div className="flex-row flex-align-center flex-justify-center" style={{ width: '100%' }}>
+                <div className="flex-row flex-align-start flex-justify-center" style={{ width: '100%' }}>
+                    <p className="heading"
+                       style={{
+                           marginBottom: '48px',
+                           marginRight: '24px',
+                           lineHeight: '1.4'
+                       }}>
+                        {t('title_chatNull2')}
+                        <br />
+                        {t('title_chatNull3')}
+                        <br />
+                        {t('title_chatNull4')}
+                    </p>
                     <img src="static/img/group-chat.png" alt=""
-                         style={{ maxWidth: '280px', minWidth: '40%' }} />
-                    <ul>
-                        <li>{t('title_chatNull2')}</li>
-                        <li>{t('title_chatNull3')}</li>
-                        {/* <li>Channels</li>*/}
-                        <li>{t('title_chatNull4')}</li>
-                    </ul>
+                         style={{ maxWidth: '280px', maxHeight: '233px', minWidth: '40%' }} />
                 </div>
             </div>
-            <div className="flex-col flex-grow-1" />
         </div>
     );
 }

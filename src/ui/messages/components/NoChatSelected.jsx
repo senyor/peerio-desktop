7fcd1dd1c59ee25ab725a5d310a844b120842f7b
Paul Dukes <paul.r.dukes@gmail.com>
2017-04-09 10:24:11 -0400

    style(chat): updates chats zero state layout.

--- src/ui/messages/components/NoChatSelected.jsx
+++ src/ui/messages/components/NoChatSelected.jsx
@@ -11,23 +11,16 @@ function NoChatSelected() {
                      left: '24px',
                      top: '80px'
                  }} alt="" />
-            <div className="flex-col flex-grow-0 flex-shrink-0">
-                <div className="flex-row flex-justify-center">
+            <div className="flex-col flex-grow-0">
+                <div className="flex-col flex-align-center">
                     <div className="display-3">{t('title_chatNull1')}</div>
+                    <p style={{
+                        marginBottom: '48px'
+                    }}>
+                        {t('title_chatNull2')}
+                    </p>
                 </div>
                 <div className="flex-row flex-align-start flex-justify-center" style={{ width: '100%' }}>
-                    <p className="heading"
-                       style={{
-                           marginBottom: '48px',
-                           marginRight: '24px',
-                           lineHeight: '1.4'
-                       }}>
-                        {t('title_chatNull2')}
-                        <br />
-                        {t('title_chatNull3')}
-                        <br />
-                        {t('title_chatNull4')}
-                    </p>
                     <img src="static/img/group-chat.png" alt=""
                          style={{ maxWidth: '280px', maxHeight: '233px', minWidth: '40%' }} />
                 </div>

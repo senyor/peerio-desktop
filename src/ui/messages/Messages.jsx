2a9509a6ff6591f78096fcad8c14853d40ecf54d
Paul Dukes <paul.r.dukes@gmail.com>
2017-04-17 09:38:17 -0400

    style(chat): adds sidebar markup, animation, and basic styling

--- src/ui/messages/Messages.jsx
+++ src/ui/messages/Messages.jsx
@@ -1,6 +1,7 @@
 const React = require('react');
-const { reaction } = require('mobx');
+const { observable, reaction } = require('mobx');
 const { observer } = require('mobx-react');
+const { IconButton, Input, List, ListItem } = require('~/react-toolbox');
 const ChatList = require('./components/ChatList');
 const MessageInput = require('./components/MessageInput');
 const MessageList = require('./components/MessageList');
@@ -8,9 +9,13 @@ const NoChatSelected = require('./components/NoChatSelected');
 const { chatStore } = require('~/icebear');
 const sounds = require('~/helpers/sounds');
 const UploadInChatProgress = require('./components/UploadInChatProgress');
+const { t } = require('peerio-translator');
+const css = require('classnames');
 
 @observer
 class Messages extends React.Component {
+    @observable sidebarOpen = false;
+
     componentWillMount() {
         if (chatStore.activeChat) chatStore.activeChat.loadMessages();
         this.loaderDisposer = reaction(() => chatStore.activeChat, () => chatStore.activeChat.loadMessages());
@@ -38,22 +43,40 @@ class Messages extends React.Component {
             .catch(() => sounds.destroy.play());
     };
 
+    handleSidebar = () => {
+        this.sidebarOpen = !this.sidebarOpen;
+    }
+
     render() {
         return (
             <div className="messages">
                 <ChatList />
                 <div className="message-view">
-                    <div className="message-toolbar">
+                    <div className="message-toolbar flex-justify-between">
                         <div className="title">{chatStore.activeChat && chatStore.activeChat.chatName}</div>
+                        <IconButton icon="chrome_reader_mode" onClick={this.handleSidebar} />
                     </div>
-                    {(chatStore.chats.length === 0 && !chatStore.loading)
-                        ? <NoChatSelected />
-                        : <MessageList />}
-                    {chatStore.activeChat && chatStore.activeChat.uploadQueue.length
-                        ? <UploadInChatProgress queue={chatStore.activeChat.uploadQueue} />
-                        : null}
-                    <MessageInput show={!!chatStore.activeChat && chatStore.activeChat.metaLoaded}
-                        onSend={this.sendMessage} onAck={this.sendAck} onFileShare={this.shareFiles} />
+                    <div className="flex-row flex-grow-1">
+                        <div className="flex-col flex-grow-1">
+                            {(chatStore.chats.length === 0 && !chatStore.loading)
+                                ? <NoChatSelected />
+                                : <MessageList />}
+                            {chatStore.activeChat && chatStore.activeChat.uploadQueue.length
+                                ? <UploadInChatProgress queue={chatStore.activeChat.uploadQueue} />
+                                : null}
+                            <MessageInput show={!!chatStore.activeChat && chatStore.activeChat.metaLoaded}
+                                onSend={this.sendMessage} onAck={this.sendAck} onFileShare={this.shareFiles} />
+                        </div>
+                        <div className={css('chat-sidebar', { open: this.sidebarOpen })}>
+                            <Input value={chatStore.activeChat && chatStore.activeChat.chatName} />
+                            <div>{t('title_Members')}</div>
+                            <List selectable>
+                                <ListItem caption="Person 1" />
+                                <ListItem caption="Person 2" />
+                                <ListItem caption="Person 2" />
+                            </List>
+                        </div>
+                    </div>
                 </div>
             </div>
         );

ba379cab9df7388f1b1cdcb6221397a6ec1b0de5
Anri Asaturov <anri82@gmail.com>
2017-05-07 15:52:15 +0300

    feat(app): undo/redo/cut/copy/paste/selectall context menu, separate main menu and menu for inputs

--- src/main-process/context-menu.js
+++ src/main-process/context-menu.js
@@ -4,23 +4,49 @@ const devtools = require('~/main-process/dev-tools');
 
 const rightClickPos = { x: 0, y: 0 };
 
+
+const inputMenu = Menu.buildFromTemplate([
+    { role: 'undo' },
+    { role: 'redo' },
+    { type: 'separator' },
+    { role: 'cut' },
+    { role: 'copy' },
+    { role: 'paste' },
+    { type: 'separator' },
+    { role: 'selectall' }
+]);
+
+const menu = new Menu();
+
+menu.append(new MenuItem({
+    label: 'Restart',
+    click() {
+        appControl.relaunch();
+    }
+}));
+
+Menu.buildFromTemplate([
+    { type: 'separator' },
+    { role: 'cut' },
+    { role: 'copy' },
+    { role: 'paste' },
+    { type: 'separator' }
+]).items.forEach(i => menu.append(i));
+
 function buildContextMenu(mainWindow) {
     console.log('Building context menu.');
-    const menu = new Menu();
-
-    menu.append(new MenuItem({
-        label: 'Restart',
-        click() {
-            appControl.relaunch();
-        }
-    }));
 
     devtools.extendContextMenu(menu, mainWindow, rightClickPos);
 
     mainWindow.webContents.on('context-menu', (e, props) => {
         rightClickPos.x = props.x;
         rightClickPos.y = props.y;
-        menu.popup(mainWindow);
+        const { selectionText, isEditable } = props;
+        if (isEditable) {
+            inputMenu.popup(mainWindow);
+        } else {
+            menu.popup(mainWindow);
+        }
     });
 }
 

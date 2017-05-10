f1a29a8ef191f3aef3008c3f30bfc22045d25479
Dmitry Chestnykh <dmitry@codingrobots.com>
2017-05-10 12:24:41 +0200

    chore(deploy): use desktop-release-builder

--- scripts/deploy-production.sh
+++ scripts/deploy-production.sh
@@ -1,18 +1,16 @@
 #!/usr/bin/env bash
-npm run get-locales
 echo "[•••            ] setting flags"
 set -e
 export NODE_ENV=production
-#export CSC_LINK=~/.cert/peerio_mac.p12
-# EV certs use token instead of file, subject is set in electron-builder config: win/certificateSubjectName
-# export WIN_CSC_LINK=~/.cert
-echo "[••••••         ] cleaning up"
-rm -rf dist
-echo "[•••••••••      ] compiling sources"
-npm run compile
-echo "[••••••••••••   ] creating github release"
+
+echo "[••••••••       ] tagging release"
 ./node_modules/.bin/standard-version
-git push --follow-tags origin master
-echo "[•••••••••••••••] building bundles"
-build --windows --ia32 --x64
-build --mac --linux --publish always
+git push && git push --follow-tags origin master
+
+echo "[•••••••••••••••] building and publishing"
+echo '**'
+echo '** NOTE: on Windows, run: peerio-desktop-signer --shared Y:\ --certificate "certificate subject name"'
+echo '**'
+peerio-desktop-release --shared ~/Win \
+                       --repository PeerioTechnologies/peerio-desktop \
+                       --publish

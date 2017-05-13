1b0f3cbf0758dc383ebf61d4d3aaf2b07ca44384
Eren Bilgin <eren@peerio.com>
2017-05-13 09:10:54 -0400

    chore(l18n): localise new signup account key screen

--- src/ui/signup/AccountKey.jsx
+++ src/ui/signup/AccountKey.jsx
@@ -23,17 +23,17 @@ const { validators } = validation; // use common validation from core
             <div className="flex-col profile">
                 <div className="warning-line">
                     <div className="flex-col flex-justify-center" style={{ width: '275px' }}>
-                        <span className="display-1">Don't get</span>
-                        <strong className="display-2">Locked out!</strong>
+                        <span className="display-1">{t('title_AKwarn1')}</span>
+                        <strong className="display-2">{t('title_AKwarn2')}</strong>
                     </div>
                 </div>
-                <p>Peerio doesn’t use <em>passwords</em> like other apps. This key protects all your info, <strong>so keep it safe!</strong></p>
+                <p>{t('title_AKwarn3')}</p>
                 <div>
-                    <p>Your Account Key</p>
+                    <p>{t('title_yourAccountKey')}</p>
                     <div className="passphrase selectable">{this.props.profileStore.passphrase}</div>
 
                 </div>
-                <p><strong>Save this now.</strong> You’re going to need it later.</p>
+                <p>{t('title_AKwarn4')}</p>
             </div>
         );
     }

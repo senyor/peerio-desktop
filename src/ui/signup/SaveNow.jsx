92c3022691fc0007aa7ccf6f396708455d9c6b36
Paul Dukes <paul.r.dukes@gmail.com>
2017-05-15 10:04:38 -0400

    style(signup): adds margin to button on save now page

--- src/ui/signup/SaveNow.jsx
+++ src/ui/signup/SaveNow.jsx
@@ -57,7 +57,11 @@ const { validators } = validation; // use common validation from core
                     <div className="passphrase">{this.props.store.passphrase}</div>
                 </div>
 
-                <Button icon="file_download" label={t('button_save')} className="button-gradient" onClick={this.save} />
+                <Button icon="file_download"
+                        label={t('button_save')}
+                        className="button-gradient"
+                        style={{ marginTop: '16px' }}
+                        onClick={this.save} />
                 <webview ref={this.wwref} src="./AccountKeyBackup.html"
                     style={{ display: 'inline-flex', width: 0, height: 0, flex: '0 1' }} />
             </div>

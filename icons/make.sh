f46ca82615423b146fda5a99a6d0c50b588b12f9
Dmitry Chestnykh <dmitry@codingrobots.com>
2017-05-15 13:04:28 -0700

    chore: new icons (with make-icons build script) (#21)

---
+++ icons/make.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+
+BASE_DIR=$(dirname "$0")
+
+# Mac
+iconutil --convert icns --output $BASE_DIR/../dist-assets/icon.icns $BASE_DIR/icon.iconset
+iconutil --convert icns --output $BASE_DIR/../dist-assets/dmg-icon.icns $BASE_DIR/dmg-icon.iconset
+
+# Windows
+convert \
+    $BASE_DIR/win-icon/icon_256x256.png \
+    $BASE_DIR/win-icon/icon_64x64.png \
+    $BASE_DIR/win-icon/icon_48x48.png \
+    $BASE_DIR/win-icon/icon_32x32.png \
+    $BASE_DIR/win-icon/icon_16x16.png \
+    $BASE_DIR/../dist-assets/win-installer.ico
+
+convert \
+    $BASE_DIR/win-installer/icon_256x256.png \
+    $BASE_DIR/win-installer/icon_64x64.png \
+    $BASE_DIR/win-installer/icon_48x48.png \
+    $BASE_DIR/win-installer/icon_32x32.png \
+    $BASE_DIR/win-installer/icon_16x16.png \
+    $BASE_DIR/../dist-assets/icon.ico
+
+# Linux window icon
+cp $BASE_DIR/icon.iconset/icon_512x512@2x.png $BASE_DIR/../src/static/img/icon.png

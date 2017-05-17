0185b7a81dbf4037ed9a60e8ba28e8dc4b77b01d
Eren Bilgin <eren@peerio.com>
2017-05-17 10:58:44 -0400

    chore(l18n): localise autologin prompt

--- src/ui/login/AutoLogin.jsx
+++ src/ui/login/AutoLogin.jsx
@@ -3,6 +3,7 @@ const { Button } = require('~/react-toolbox');
 const { TinyDb } = require('~/icebear');
 const { observer } = require('mobx-react');
 const autologin = require('~/helpers/autologin');
+const { t } = require('peerio-translator');
 
 @observer class AutoLogin extends React.Component {
     enable() {
@@ -20,18 +21,18 @@ const autologin = require('~/helpers/autologin');
             <div className="auto-sign-in rt-light-theme" >
                 <div className="flex-align-center flex-col">
                     <img alt="peerio" className="logo" src="static/img/logo-white.png" />
-                    <div className="display-2">Enable Automatic Sign-in?</div>
+                    <div className="display-2">{t('title_enableAutomatic')}</div>
                     <div className="options">
                         <div className="option">
-                            <Button label="enable" value="enable" className="button-gradient" onClick={this.enable} />
+                            <Button label={t('button_enable')} value="enable" className="button-gradient" onClick={this.enable} />
                             <p>
-                                <em>Account Key</em> is never required to sign in. Sign out to disable.
+                                {t('title_enableAutomatic1')}
                             </p>
                         </div>
                         <div className="option">
-                            <Button label="disable" value="disable" onClick={this.disable} />
+                            <Button label={t('button_disable')} value="disable" onClick={this.disable} />
                             <p>
-                                <em>Account Key</em> is always required to sign in.
+                                {t('title_enableAutomatic2')}
                             </p>
                         </div>
                     </div>

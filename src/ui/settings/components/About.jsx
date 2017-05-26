446a8159c99f8e22b1f58816f31ec2f078a86c17
Paul Dukes <paul.r.dukes@gmail.com>
2017-05-26 10:27:31 -0400

    style(whitelabel): updates styling so that we whitelabel stuff.

--- src/ui/settings/components/About.jsx
+++ src/ui/settings/components/About.jsx
@@ -1,7 +1,7 @@
 const React = require('react');
 const { observable, when } = require('mobx');
 const { observer } = require('mobx-react');
-const { Button, Dialog, FontIcon, IconButton, TooltipIconButton } = require('~/react-toolbox');
+const { Button, Dialog, TooltipIconButton } = require('~/react-toolbox');
 const { t } = require('peerio-translator');
 const T = require('../../shared-components/T');
 const version = require('electron').remote.app.getVersion();

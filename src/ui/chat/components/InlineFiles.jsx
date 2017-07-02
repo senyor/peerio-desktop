a3674fea97a92a40360fba51d27ee2f37c70362e
Anri Asaturov <anri82@gmail.com>
2017-07-02 18:29:18 +0300

    fix(ux): chat inline files display 'processing...' for some time before giving up and showing 'file unavailable'

--- src/ui/chat/components/InlineFiles.jsx
+++ src/ui/chat/components/InlineFiles.jsx
@@ -2,11 +2,26 @@ const React = require('react');
 const { fileStore } = require('~/icebear');
 const { FontIcon, ProgressBar } = require('~/react-toolbox');
 const { downloadFile } = require('~/helpers/file');
+const { observable } = require('mobx');
 const { observer } = require('mobx-react');
 const { t } = require('peerio-translator');
 
 @observer
 class InlineFiles extends React.Component {
+    // give up waiting for file object to appear in store
+    @observable giveUp = false;
+
+    startTimer() {
+        if (this.timer) return;
+        this.timer = setTimeout(() => {
+            this.giveUp = true;
+        }, 10000);
+    }
+
+    componentWillUnmount() {
+        if (this.timer) clearTimeout(this.timer);
+    }
+
     download(ev) {
         const attr = ev.currentTarget.attributes['data-id'];
         if (!attr) return;
@@ -14,6 +29,7 @@ class InlineFiles extends React.Component {
         if (!file || file.downloading) return;
         downloadFile(file);
     }
+
     render() {
         if (!this.props.files.map) return null;
         return (
@@ -21,7 +37,12 @@ class InlineFiles extends React.Component {
                 {
                     this.props.files.map(f => {
                         const file = fileStore.getById(f);
-                        if (!file) return <div className="invalid-file" key={f}>{t('error_fileRemoved')}</div>;
+                        if (!file) {
+                            this.startTimer();
+                            return (<div className="invalid-file" key={f}>
+                                {t(this.giveUp ? 'error_fileRemoved' : 'title_processing')}
+                            </div>);
+                        }
                         return (<div className="shared-file" key={f} data-id={f} onClick={this.download}>
                             <div className="flex-row flex-align-center">
                                 <div className="file-name"> {file ? file.name : f}</div>

f6032808fe0c2b09e8c301843f82b89e24f25b6e
Anri Asaturov <anri82@gmail.com>
2017-07-18 00:32:42 -0400

    chore: add timestamp to log lines

--- src/helpers/console-history.js
+++ src/helpers/console-history.js
@@ -10,8 +10,8 @@ const originals = {
 
 console.history = {
     cache: [],
-    write(args) {
-        let line = '';
+    write(timestamp, args) {
+        let line = timestamp;
         for (let i = 0; i < args.length; i++) {
             if (typeof args[i] === 'object') {
                 line += `${safeJsonStringify(args[i])} `;
@@ -32,19 +32,23 @@ console.history = {
 
 const h = console.history;
 
-console.log = function(...args) {
-    originals.log.apply(console, args);
-    h.write(args);
+console.log = function (...args) {
+    const t = new Date().toISOString();
+    originals.log.call(console, t, ...args);
+    h.write(t, args);
 };
-console.warn = function(...args) {
-    originals.warn.apply(console, args);
-    h.write(args);
+console.warn = function (...args) {
+    const t = new Date().toISOString();
+    originals.warn.call(console, t, ...args);
+    h.write(t, args);
 };
-console.error = function(...args) {
-    originals.error.apply(console, args);
-    h.write(args);
+console.error = function (...args) {
+    const t = new Date().toISOString();
+    originals.error.call(console, t, ...args);
+    h.write(t, args);
 };
-console.debug = function(...args) {
-    originals.debug.apply(console, args);
-    h.write(args);
+console.debug = function (...args) {
+    const t = new Date().toISOString();
+    originals.debug.call(console, t, ...args);
+    h.write(t, args);
 };

e440ca5cc453a23a760e8a12bc1cf7b87b7a42b5
Anri Asaturov <anri82@gmail.com>
2017-08-22 12:29:46 +0300

    chore: remove obsolete code from security settings view

--- src/ui/settings/components/SecuritySettings.jsx
+++ src/ui/settings/components/SecuritySettings.jsx
@@ -11,55 +11,14 @@ const electron = require('electron').remote;
 @observer
 class SecuritySettings extends React.Component {
 
-    @observable passphraseDialogOpen = false;
-    @observable unlocked = false;
-    @observable setupTwoFactorDialogOpen = false;
-    @observable twoFactorActive = false;
-    @observable backupCodesDialogOpen = false;
     @observable passphraseVisible = false;
-
-    unlock = () => {
-        this.unlocked = true;
-    };
-
-    hidePassphraseDialog = () => {
-        this.passphraseDialogOpen = false;
-        this.unlocked = false;
-    };
-
-    showPassphraseDialog = () => {
-        this.passphraseDialogOpen = true;
-    };
 
     togglePassphraseVisibility = () => {
         this.passphraseVisible = !this.passphraseVisible;
     };
 
-
-    hideTwoFactorDialog = () => {
-        this.setupTwoFactorDialogOpen = false;
-        this.deactivateTwoFactor();
-    };
-
-    showTwoFactorDialog = () => {
-        this.setupTwoFactorDialogOpen = true;
-    };
-
-    deactivateTwoFactor = () => {
-        // TODO trigger confirm dialog.
-        this.twoFactorActive = !this.twoFactorActive;
-    };
-
-    hideBackupCodesDialog = () => {
-        this.backupCodesDialogOpen = false;
-    };
-
-    showBackupCodesDialog = () => {
-        this.backupCodesDialogOpen = true;
-    };
-
-    wwref = (ref) => {
-        this.webviewref = ref;
+    setWebViewRef = (ref) => {
+        this.webViewRef = ref;
     }
     // TODO: this is duplicated with SaveNow, FIX IT
     save = () => {
@@ -75,22 +34,18 @@ class SecuritySettings extends React.Component {
             document.getElementById('email').innerHTML = '${email}';
             document.getElementById('key').innerHTML = '${key}';
             `;
-        this.webviewref.executeJavaScript(js, true, callback);
+        this.webViewRef.executeJavaScript(js, true, callback);
     }
 
     printToPdf = (filePath) => {
         if (!filePath) return;
-        this.webviewref.printToPDF({ printBackground: true, landscape: false }, (er, data) => {
+        this.webViewRef.printToPDF({ printBackground: true, landscape: false }, (er, data) => {
             fs.writeFileSync(filePath, data);
         });
     };
 
 
     renderShowPassphraseSection() {
-        // const passphraseDialogActions = [
-        //     { label: t('button_close'), onClick: this.hidePassphraseDialog }
-        // ];
-
         return (
             <div>
                 <div className="title">
@@ -117,23 +72,10 @@ class SecuritySettings extends React.Component {
                         primary />
                 </div>
 
-                <webview ref={this.wwref} src="./AccountKeyBackup.html"
+                <webview ref={this.setWebViewRef} src="./AccountKeyBackup.html"
                     style={{ display: 'inline-flex', width: 0, height: 0, flex: '0 1' }} />
 
-                {/* <Dialog active={this.passphraseDialogOpen} actions={passphraseDialogActions}
-                    className="no-select" title={this.unlocked ? t('title_AccountKey') : t('title_enterPassword')}
-                    onOverlayClick={this.hidePassphraseDialog} onEscKeyDown={this.hidePassphraseDialog}>
-                    {this.unlocked ?
-                        <div>
-                            <p>{t('title_AKDetail2')}</p>
-                            <div style={{ marginTop: '40px', height: '48px' }}>
-                                <div className="passphrase headline selectable">{User.current.passphrase}</div>
-                            </div>
-                        </div>
-                        : <PasscodeLock onUnlocked={this.unlock} />}
-
-                </Dialog> */}
-            </div >
+            </div>
         );
     }
 
@@ -142,25 +84,9 @@ class SecuritySettings extends React.Component {
     }
 
     render() {
-        // const backupCodesDialogActions = [
-        //     { label: t('button_cancel'), onClick: this.hideBackupCodesDialog },
-        //     { label: t('button_save'), onClick: this.hideBackupCodesDialog }
-        // ];
-        // const twoFactorDialogActions = [
-        //     { label: t('button_cancel'), onClick: this.hideTwoFactorDialog },
-        //     { label: t('button_enable2FA'), onClick: this.hideTwoFactorDialog }
-        // ];
-
         return (
             <div>
-                {/* NOTE: Account key should always be at the top.  */}
                 <section className="section-divider section-account-key">
-                    {/* <div className="title">{t('title_QRcode')}
-                        <Button label={t('button_showQR')} flat primary />
-                    </div>
-                    <p style={{ marginBottom: '40px' }}>
-                        {t('title_QRcodeDetail')}
-                    </p> */}
                     {this.renderShowPassphraseSection()}
                 </section>
                 <section>
@@ -174,72 +100,6 @@ class SecuritySettings extends React.Component {
                         label={t('title_autologinSetting')}
                         onChange={this.onToggleAutologin} />
                 </section>
-                {/* <section className="section-divider">
-                    <div className="title" >
-                        {t('title_devicePassword')}
-                        <Button label={t('button_changePassword')} flat primary />
-                    </div>
-                    <p style={{ marginBottom: '40px' }}>
-                        <T k="title_passwordIntro" >
-                            {{
-                                emphasis: text => <strong>{text}</strong>
-                            }}
-                        </T>
-                    </p>
-                    <div className="title">{t('title_2FA')}
-                        <Button label={this.twoFactorActive ?
-                                t('button_backupCodes') : t('button_enable2FA')}
-                                onClick={this.twoFactorActive ?
-                                  this.showBackupCodesDialog : this.showTwoFactorDialog}
-                                flat primary />
-                        { this.twoFactorActive ?
-                            <Button label={t('button_disable2FA')}
-                                      onClick={this.deactivateTwoFactor}
-                                      flat primary /> : null }
-                    </div>
-                    <p>
-                        {t('title_2FADetail', { tfaDetailUrl: text => <a href={cfg.tfaDetailUrl}>{text}</a> })}
-                    </p>
-                </section> */}
-
-                {/* <section>
-                    <div className="title"> {t('title_accountActivity')}
-                        <Button label={t('button_showActivity')} flat primary />
-                    </div>
-                    <p>
-                        {t('title_accountActivityDetail')}
-                    </p>
-                </section> */}
-                {/* Technically speaking: do we want 2 dialogs or 1 dialog with steps? */}
-                {/* <Dialog active={this.setupTwoFactorDialogOpen} actions={twoFactorDialogActions}
-                        onOverlayClick={this.hideTwoFactorDialog} onEscKeyDown={this.hideTwoFactorDialog}
-                        title={t('title_2FASetupDesktop')}>
-                    <div>
-                        <p>{t('title_2FASetupDesktopDetail')}</p>
-                        <div>{t('title_2FASetupDesktopDetail2')}</div>
-                        <div className="flex-row flex-align-center flex-justify-between" />
-                        <Input label={t('title_2FACode')} />
-                    </div>
-                </Dialog>
-
-                <Dialog active={this.backupCodesDialogOpen} actions={backupCodesDialogActions}
-                        onOverlayClick={this.hideBackupCodesDialog} onEscKeyDown={this.hideBackupCodesDialog}
-                        title={t('title_backupCodes')}>
-                    <div>
-                        <p>{t('title_backupCodesDetail')}</p>
-                        <div className="flex-col">
-                            <div className="backup-row">
-                                <strong>123456</strong><strong>123456</strong>
-                            </div>
-                            <div className="backup-row">
-                                <strong>123456</strong><strong>123456</strong>
-                            </div>
-                            <div className="backup-row">
-                                <strong>123456</strong><strong>123456</strong>
-                            </div>
-                        </div>
-                    </div>
-                </Dialog> */}
             </div>
         );
     }

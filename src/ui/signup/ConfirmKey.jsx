09697d7ead1ae3215f6fe5233519fa3db8e7b314
Samvel Avanesov <seavan@gmail.com>
2017-09-04 21:46:00 -0400

    fix: add handleKeyPress to confirmkey enter

--- src/ui/signup/ConfirmKey.jsx
+++ src/ui/signup/ConfirmKey.jsx
@@ -37,7 +37,9 @@ const T = require('~/ui/shared-components/T');
                     {/* If you lose it, we cannot help you access your account. */}
                     <p>Confirm that you’ve saved your key by entering  “{this.confirmTextSample}” in the input below. </p>
                     {/* Hey Samvel, I'm not sure how to create a key for this. */}
-                    <Input value={this.confirmText} label="Have you saved your account key?" onChange={text => { this.confirmText = text; }} />
+                    <Input
+                        onKeyPress={this.handleKeyPress}
+                        value={this.confirmText} label="Have you saved your account key?" onChange={text => { this.confirmText = text; }} />
                 </div>
             </div>
         );

3cc73494e6d651233534743d6c3ae177f79e505d
Anri Asaturov <anri82@gmail.com>
2017-11-15 05:43:50 +0200

    chore: fix hooks

--- scripts/npm_install_if_changed.js
+++ scripts/npm_install_if_changed.js
@@ -1,22 +1,21 @@
 const fs = require('fs');
-const { execSync } = require('child_process');
+const { execSync, spawnSync } = require('child_process');
 
 try {
-    let existing, existingApp;
-
+    let existing = '', appExisting = '';
     try {
-        existing = fs.readFileSync('.package.json.md5');
-        existingApp = fs.readFileSync('.app.package.json.md5');
+        existing = fs.readFileSync('.package.json.md5') || '';
+        appExisting = fs.readFileSync('.app.package.json.md5') || '';
     } catch (ex) {
         // don't care
     }
-
     const actual = execSync(process.platform === 'darwin' ? 'md5 -q package.json' : 'md5sum package.json');
-    const actualApp = execSync(process.platform === 'darwin' ? 'md5 -q app/package.json' : 'md5sum app/package.json');
-
-    if (existing.toString() !== actual.toString() || existingApp.toString() !== actualApp.toString()) {
+    const appActual = execSync(process.platform === 'darwin' ? 'md5 -q app/package.json' : 'md5sum app/package.json');
+    if (existing.toString() !== actual.toString() || appExisting.toString() !== appActual.toString()) {
         console.log('package.json has changed, running npm install.');
-        execSync('npm install');
+        const ret = spawnSync('npm', ['install']);
+        console.log(ret.stderr && ret.stderr.toString());
+        console.log(ret.stdout && ret.stdout.toString());
     } else {
         console.log('package.json has not changed, skipping npm install.');
     }

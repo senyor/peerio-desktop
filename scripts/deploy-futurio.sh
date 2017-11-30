9a9641b3d20f488a95c87b7d250aa81b6ab29875
Dmitry Chestnykh <dmitry@codingrobots.com>
2017-11-30 02:37:59 +0100

    chore: bring back npm install to deploy-futurio (#163)

--- scripts/deploy-futurio.sh
+++ scripts/deploy-futurio.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+set -e
+
+npm install
+
+export NODE_ENV=production
+
+
 echo "Building and publishing futurio"
 ./node_modules/.bin/peerio-desktop-release --repository PeerioTechnologies/peerio-desktop \
                        --overrides PeerioTechnologies/peerio-desktop-futurio \

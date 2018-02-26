85239e48a610c28212b72f1fdb8d1a0418ed543a
Lucas <lucas@superko.org>
2018-02-26 11:44:42 -0500

    refactor: make MigrationDialog separate component

--- src/ui/App.jsx
+++ src/ui/App.jsx
@@ -1,13 +1,14 @@
 const React = require('react');
-const AppNav = require('~/ui/AppNav');
-const uiStore = require('~/stores/ui-store');
-const { Dialog, ProgressBar } = require('~/peer-ui');
-const T = require('~/ui/shared-components/T');
-const { t } = require('peerio-translator');
-const ContactProfile = require('~/ui/contact/components/ContactProfile');
 const { observer } = require('mobx-react');
-const { action, observable } = require('mobx');
-const { clientApp, warnings } = require('peerio-icebear');
+const { observable } = require('mobx');
+
+const uiStore = require('~/stores/ui-store');
+const { clientApp } = require('peerio-icebear');
+const { t } = require('peerio-translator');
+const { Dialog, ProgressBar } = require('~/peer-ui');
+const AppNav = require('~/ui/AppNav');
+const ContactProfile = require('~/ui/contact/components/ContactProfile');
+const MigrationDialog = require('~/ui/shared-components/MigrationDialog');
 
 @observer
 class App extends React.Component {
@@ -35,71 +36,6 @@ class App extends React.Component {
                 title={t('title_invalidFileSignature')}
                 className="dialog-warning">
                 <p>{t('error_invalidFileSignatureLong')}</p>
-            </Dialog>
-        );
-    }
-
-    @observable migrationDialogVisible = true;
-    @observable unshareSelected = false;
-
-    sharedFiles = {
-        fileId: '',
-        filename: 'filename.txt'
-    };
-
-    @action.bound unshare() {
-        this.unshareSelected = true;
-    }
-
-    @action.bound continueMigration() {
-        this.migrationDialogVisible = false;
-        warnings.add(t('warning_sharingHistory'));
-    }
-
-    @action.bound cancelUnshare() {
-        this.unshareSelected = false;
-    }
-
-    @action.bound continueUnshare() {
-        this.migrationDialogVisible = false;
-        warnings.add(t('warning_unsharedAll'));
-    }
-
-    downloadFile = () => {
-        console.log('download file');
-    }
-
-    get migrationDialog() {
-        const migrationDialogActions = [
-            { label: t('button_no'), onClick: this.unshare },
-            { label: t('button_yes'), onClick: this.continueMigration }
-        ];
-
-        const unshareDialogActions = [
-            { label: t('button_cancel'), onClick: this.cancelUnshare },
-            { label: t('button_unshareAll'), onClick: this.continueUnshare }
-        ];
-
-        const textParser = {
-            downloadFile: () => <a className="clickable" onClick={this.downloadFile}>{this.sharedFiles.filename}</a>
-        };
-
-        return (
-            <Dialog active={this.migrationDialogVisible}
-                actions={this.unshareSelected
-                    ? unshareDialogActions
-                    : migrationDialogActions
-                }
-                title={this.unshareSelected
-                    ? t('dialog_unshareTitle')
-                    : t('dialog_migrationTitle')
-                }
-                theme="warning"
-            >
-                {this.unshareSelected
-                    ? <T k="dialog_unshareText" />
-                    : <T k="dialog_migrationText">{textParser}</T>
-                }
             </Dialog>
         );
     }
@@ -132,7 +68,7 @@ class App extends React.Component {
                             : null
                     }
                 </Dialog>
-                {this.migrationDialog}
+                <MigrationDialog />
                 {this.signatureErrorDialog}
             </div>
         );

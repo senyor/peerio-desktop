0a147f18c45d570c64d8c2f4129edb11436178a1
Anri Asaturov <anri82@gmail.com>
2018-03-25 17:25:16 +0300

    fix: null ref

--- src/ui/files/Files.jsx
+++ src/ui/files/Files.jsx
@@ -408,7 +408,7 @@ class Files extends React.Component {
     refConfirmFolderDeleteDialog = ref => { fileStore.bulk.deleteFolderConfirmator = ref && ref.show; };
 
     render() {
-        if (!fileStore.files.length && !fileStore.virtualFolders.length
+        if (!fileStore.files.length && !fileStore.folders.root.virtualFolders.length
             && !fileStore.loading) return <ZeroScreen onUpload={this.handleUpload} />;
 
         const { currentFolder } = fileStore.folders;

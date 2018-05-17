7819754d191aea05c13d75683c66e515cb1c9a59
suprko <lucas@peerio.com>
2018-05-17 16:08:53 -0400

    feat: random messages during migration-in-progress

--- src/ui/shared-components/MigrationDialog.jsx
+++ src/ui/shared-components/MigrationDialog.jsx
@@ -1,14 +1,59 @@
 const React = require('react');
+const { computed, observable, reaction } = require('mobx');
 const { observer } = require('mobx-react');
 const { User, fileStore } = require('peerio-icebear');
 const T = require('~/ui/shared-components/T');
 const { t } = require('peerio-translator');
 const { Dialog, ProgressBar } = require('~/peer-ui');
+const EmojiImage = require('~/ui/emoji/Image');
 const electron = require('electron').remote;
 const fs = require('fs');
 
+const migrationInProgressMessages = [];
+const MIGRATION_MESSAGES_COUNT = 10;
+const MESSAGE_SPEED = 8000;
+
+for (let i = 1; i <= MIGRATION_MESSAGES_COUNT; i++) {
+    const random = Math.floor(Math.random() * migrationInProgressMessages.length);
+
+    if (i === 3) {
+        migrationInProgressMessages.splice(random, 0, (
+            <span>
+                <T k={`title_migrationInProgressMessage${i}`} />&nbsp;
+                <EmojiImage emoji="smile_cat" />
+            </span>
+        ));
+    } else if (i === 5) {
+        migrationInProgressMessages.splice(random, 0, (
+            <span>
+                <T k={`title_migrationInProgressMessage${i}a`} />&nbsp;
+                <EmojiImage emoji="muscle" />.&nbsp;
+                <T k={`title_migrationInProgressMessage${i}b`} />
+            </span>
+        ));
+    } else {
+        migrationInProgressMessages.splice(random, 0, (
+            <T k={`title_migrationInProgressMessage${i}`} />
+        ));
+    }
+}
+
 @observer
 class MigrationDialog extends React.Component {
+    componentWillMount() {
+        this.dispose = reaction(
+            () => !fileStore.migration.started && !fileStore.migration.performedByAnotherClient,
+            () => this.clearTimer()
+        );
+    }
+
+    componentWillUnmount() {
+        this.clearTimer();
+        this.reactionsToDispose.forEach(dispose => {
+            if (dispose) dispose();
+        });
+    }
+
     downloadFile = () => {
         const win = electron.getCurrentWindow();
         electron.dialog.showSaveDialog(win, {
@@ -25,7 +70,29 @@ class MigrationDialog extends React.Component {
         download: text => <a className="clickable" onClick={this.downloadFile}>{text}</a>
     }
 
+    @observable count = -1;
+    @computed get currentMessage() {
+        return migrationInProgressMessages[this.count];
+    }
+
+    clearTimer() {
+        if (this.timer) {
+            clearInterval(this.timer);
+            this.timer = null;
+        }
+    }
+
     renderProgress() {
+        if (!this.timer) {
+            this.timer = setInterval(() => {
+                if (this.count < migrationInProgressMessages.length - 1) {
+                    this.count += 1;
+                } else {
+                    this.count = 0;
+                }
+            }, MESSAGE_SPEED);
+        }
+
         if (fileStore.migration.performedByAnotherClient) {
             return (
                 <div className="update-in-progress">
@@ -34,11 +101,17 @@ class MigrationDialog extends React.Component {
                 </div>
             );
         }
+
         return (
             <div className="update-in-progress">
                 <ProgressBar mode="determinate" value={fileStore.migration.progress} max={100} />
                 <div className="percent">{fileStore.migration.progress}%</div>
-                <T k="title_fileUpdateProgressDescription" tag="p" className="text" />
+                <p className="text">
+                    {this.count >= 0
+                        ? this.currentMessage
+                        : <T k="title_fileUpdateProgressDescription" />
+                    }
+                </p>
             </div>
         );
     }

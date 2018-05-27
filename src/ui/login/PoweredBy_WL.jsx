961291af904ab71e2c0dc64b63f6a67571675913
Alexandra Marin <alexandra.marin01@gmail.com>
2018-05-27 14:48:14 +0300

    fix: powered by style

--- src/ui/login/PoweredBy_WL.jsx
+++ src/ui/login/PoweredBy_WL.jsx
@@ -1,9 +1,17 @@
 const React = require('react');
 
 class PoweredBy extends React.Component {
+    style = {
+        display: 'block',
+        width: '25%'
+    };
+
     render() {
         return (
-            <img alt="Powered by Peerio" src="static/img/poweredByPeerio_white.png" />
+            <img
+                style={this.style}
+                alt="Powered by Peerio"
+                src="static/img/poweredByPeerio_white.png" />
         );
     }
 }

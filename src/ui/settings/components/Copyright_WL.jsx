d1dde95f05eb10d461d84407b7e0fb9a8bfe7d96
suprko <lucas@peerio.com>
2018-06-01 14:59:29 -0400

    fix: swap Copyright and Copyright_WL content

--- src/ui/settings/components/Copyright_WL.jsx
+++ src/ui/settings/components/Copyright_WL.jsx
@@ -4,7 +4,7 @@ const React = require('react');
 class Copyright extends React.Component {
     render() {
         return (
-            <span>&copy; 2017 Peerio Technologies, Inc. All rights reserved.</span>
+            <span />
         );
     }
 }

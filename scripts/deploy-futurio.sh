f8ba638883d6b373f7086545db39f74176c2adcf
Anri Asaturov <anri82@gmail.com>
2018-06-04 21:40:21 +0300

    Update deploy-futurio.sh

--- scripts/deploy-futurio.sh
+++ scripts/deploy-futurio.sh
@@ -11,7 +11,7 @@ echo "Building and publishing futurio"
 ./node_modules/.bin/peerio-desktop-release --repository PeerioTechnologies/peerio-desktop \
                        --overrides PeerioTechnologies/peerio-desktop-futurio \
                        --publish \
-                       --tag newfs \
+                       --tag dev \
                        --versioning futurio \
                        --nosign \
                        --key ../peerio-desktop-futurio/signing.key

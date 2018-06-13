senyor/peerio-desktop

commit 8141f3e74729f7454a77b1f7e4e05538a22bdc00

feat: improved updater (#287)
* chore(updater): set downloads directory to Updates in userData

* chore(updater): remove old unnecessary code

We don't use Squirrel.Windows for a long time.

* feat: handle update failures

When starting, Peerio checks if the last update failed (this is designed
to happen when starting a new version right after the update), and if
so, it will show the error to the user and ask them to try the update
again, download the new version manually, or continue use the current
(older) version.

This also changes the "update is ready to install" dialog from alert()
to PeerUI Dialog and introduces another dialog to indicate that the
update is in progress.

Languages are now loaded before mounting the root component to avoid
race condition when something may have been showed before translations
are initialized.

* feat(updater): force check and install if client is deprecated

* feat: install updates on quit, ask to update every 12 hours

When the update is downloaded and it's not mandatory, it's automatically
scheduled to be installed when the user quits the app. If the user
doesn't quit within 12 hours, they are asked to install update (via a
dialog), with an option to postpone ("Later" button). The user will be
asked again in 12 hours, up to 2 times. The third time the dialog
appears, there's no "Later" option and the user will have to update.

If the update is mandatory, the user is asked to install it immediately
without an option to postpone.

* fix(updater): use isMandatorySince()

* fix(updater): pass update-downloaded event args correctly

* fix(updater): import app from remote

* fix(updater): pass mandatory status with update-downloaded event

* fix(updater): swap Later/Update button order

* chore(deps): bump desktop-release-builder version

* fix(updater): scheduleInstallOnQuit even for mandatory

* fix(updater): do not check for updates if last update failed

src/ui/Root.jsx
@@ -1,5 +1,4 @@
 const React = require('react');
-const languageStore = require('~/stores/language-store');
 const isDevEnv = require('~/helpers/is-dev-env');
 const config = require('~/config');
 const { setStringReplacement } = require('peerio-translator');
@@ -13,6 +12,9 @@ const { t } = require('peerio-translator');
 const SystemWarningDialog = require('~/ui/shared-components/SystemWarningDialog');
 const TwoFADialog = require('~/ui/shared-components/TwoFADialog');
 const Snackbar = require('~/ui/shared-components/Snackbar');
+const UpdateFailedDialog = require('./updater/UpdateFailedDialog');
+const InstallingUpdateDialog = require('./updater/InstallingUpdateDialog');
+const ReadyToInstallUpdateDialog = require('./updater/ReadyToInstallUpdateDialog');
 const routerStore = require('~/stores/router-store');
 const appState = require('~/stores/app-state');
 const appControl = require('~/helpers/app-control');
@@ -36,18 +38,13 @@ class Root extends React.Component {
         config.translator.stringReplacements.forEach((replacementObject) => {
             setStringReplacement(replacementObject.original, replacementObject.replacement);
         });
-        // UI language handling
-        languageStore.loadSavedLanguage();
         // events from main process
-        ipcRenderer.on('warning', (ev, key) => warnings.add(key));
-        ipcRenderer.on('update-will-restart', () => {
-            alert(t('title_updateWillRestart'));
-            ipcRenderer.send('install-update');
-        });
+        ipcRenderer.on('warning', (ev, key) => warnings.add(key)); // TODO: not needed anymore?
         ipcRenderer.on('console_log', (ev, arg) => console.log(arg));
         ipcRenderer.on('activate_dev_mode', () => {
             appState.devModeEnabled = true;
         });
+
         reaction(() => socket.connected, (connected) => {
             if (connected) {
                 this.showOfflineNotification = false;
@@ -96,6 +93,9 @@ class Root extends React.Component {
                 {this.snackbarVisible ? <Snackbar /> : null}
                 <SystemWarningDialog />
                 <TwoFADialog />
+                <UpdateFailedDialog />
+                <ReadyToInstallUpdateDialog />
+                <InstallingUpdateDialog />
                 <DropTarget />
             </div>
         );

scripts/deploy-production.sh
@@ -32,6 +32,12 @@ echo "[••••••••       ] tagging release"
 ./node_modules/.bin/standard-version
 git push && git push --follow-tags origin master
 
+if [[ $1 = "mandatory" ]]; then
+  node ./scripts/update_mandatory_version
+else
+  echo "Optional update"
+fi
+
 echo "[•••••••••••••••] building and publishing"
 echo '**'
 echo '** NOTE: on Windows, run: peerio-desktop-signer --shared Y:\ --certificate "certificate subject name"'

scripts/update_mandatory_version.js
@@ -1,0 +1,11 @@
+/**
+ * Sets 'lastMandatoryUpdateVersion' in package.json to the same value as 'version'.
+ */
+const path = require('path');
+const fs = require('fs');
+
+const packageFilename = path.join(__dirname, '..', 'package.json');
+const data = require(packageFilename);
+data.lastMandatoryUpdateVersion = data.version;
+fs.writeFileSync(packageFilename, JSON.stringify(data, null, 2) + '\n');
+console.log(`Mandatory update version set to ${data.lastMandatoryUpdateVersion}`);

src/index.js
@@ -1,6 +1,8 @@
 const isDevEnv = require('~/helpers/is-dev-env');
 if (!isDevEnv) require('~/helpers/console-history');
 const { ipcRenderer, webFrame } = require('electron');
+const { when } = require('mobx');
+const languageStore = require('~/stores/language-store');
 
 // apply desktop config values to icebear
 require('~/config');
@@ -37,17 +39,22 @@ document.addEventListener('DOMContentLoaded', () => {
 
     socket.start();
 
-    render(React.createElement(Router, { history: window.router, routes }), document.getElementById('root'));
+    // Load translations and render once they're loaded.
+    languageStore.loadSavedLanguage();
+    when(() => languageStore.language, () => {
+        render(React.createElement(Router, { history: window.router, routes }), document.getElementById('root'));
 
-    ipcRenderer.on('router', (event, message) => {
-        window.router.push(message);
+        ipcRenderer.on('router', (event, message) => {
+            window.router.push(message);
+        });
+
+        // starting power management
+        require('~/helpers/power').start();
+        // starting network management
+        require('~/helpers/network').start();
+        // starting failed image reload management
+        require('~/helpers/image-retry').start();
     });
-    // starting power management
-    require('~/helpers/power').start();
-    // starting network management
-    require('~/helpers/network').start();
-    // starting failed image reload management
-    require('~/helpers/image-retry').start();
 });
 
 // Disable zoom.

src/main-process/handle-windows-update.js
@@ -1,59 +1,0 @@
-const { app } = require('electron');
-
-
-/**
- * Almost completely stock squirrel installer code. Runs the installer and seamlessly installs the app.
- * @returns {boolean} true if squirrel update is a go
- */
-function handleSquirrelEvent() {
-    // TODO: no OS check?
-    if (process.platform !== 'win32' || process.argv.length === 1) {
-        return false;
-    }
-
-    const ChildProcess = require('child_process');
-    const path = require('path');
-    const appFolder = path.resolve(process.execPath, '..');
-    const rootAtomFolder = path.resolve(appFolder, '..');
-    const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
-    const exeName = path.basename(process.execPath);
-
-    const spawn = function(command, args) {
-        let spawnedProcess;
-
-        try {
-            spawnedProcess = ChildProcess.spawn(command, args, { detached: true });
-        } catch (err) {
-            console.error(err);
-        }
-
-        return spawnedProcess;
-    };
-
-    const spawnUpdate = function(args) {
-        return spawn(updateDotExe, args);
-    };
-
-    const squirrelEvent = process.argv[1];
-    switch (squirrelEvent) {
-        case '--squirrel-firstrun':
-            // todo: whatever we might need to do on first run on windows app
-            return false;
-        case '--squirrel-install':
-        case '--squirrel-updated':
-            spawnUpdate(['--createShortcut', exeName]);
-            setTimeout(app.quit, 1000);
-            return true;
-        case '--squirrel-uninstall':
-            spawnUpdate(['--removeShortcut', exeName]);
-            setTimeout(app.quit, 1000);
-            return true;
-        case '--squirrel-obsolete':
-            app.quit();
-            return true;
-        default:
-            return false;
-    }
-}
-
-module.exports = handleSquirrelEvent();

src/main-process/updater.js
@@ -1,4 +1,4 @@
-
+const path = require('path');
 const isDevEnv = require('~/helpers/is-dev-env');
 const { app, ipcMain } = require('electron');
 const TinyDb = require('peerio-icebear/dist/db/tiny-db');
@@ -12,62 +12,90 @@ TinyDb.system.getValue('pref_prereleaseUpdatesEnabled')
         console.error('Failed to retrieve prerelease update setting from TinyDb', err);
     });
 
-let window;
-
-function sendStatusToWindow(text) {
-    console.log(text);
-    window.webContents.send('console_log', text);
-}
-
 function start(mainWindow) {
+    function sendStatusToMainWindow(text) {
+        console.log(text);
+        mainWindow.webContents.send('console_log', text);
+    }
+
     try {
-        window = mainWindow;
-        ipcMain.on('install-update', () => {
+        autoUpdater.setDownloadsDirectory(path.join(app.getPath('userData'), 'Updates'));
+
+        // Handlers for events from renderer process.
+
+        ipcMain.on('update-check', () => {
+            autoUpdater.checkForUpdates();
+        });
+
+        ipcMain.on('update-install', () => {
             console.log('Client approved update installation.');
             app.releaseSingleInstance();
             autoUpdater.quitAndInstall();
         });
+
+        ipcMain.on('update-schedule-install', () => {
+            console.log('Scheduling update installation on quit');
+            autoUpdater.scheduleInstallOnQuit();
+        });
+
+        ipcMain.on('update-retry-install', () => {
+            console.log('Retrying update installation.');
+            app.releaseSingleInstance();
+            autoUpdater.quitAndRetryInstall();
+        });
+
+        ipcMain.on('update-check-last-failed', async () => {
+            if (!autoUpdater) return;
+
+            const failed = await autoUpdater.didLastUpdateFail();
+            if (!failed) {
+                autoUpdater.cleanup(); // don't care to await it
+            }
+            mainWindow.webContents.send('update-last-failed', failed);
+        });
+
+        ipcMain.on('update-cleanup', async () => {
+            await autoUpdater.cleanup();
+            mainWindow.webContents.send('update-cleanup-done');
+        });
+
+        // Redirect updater events to renderer process.
+
+        autoUpdater.on('checking-for-update', () => {
+            mainWindow.webContents.send('checking-for-update');
+        });
+
+        autoUpdater.on('update-available', info => {
+            mainWindow.webContents.send('update-available', info);
+        });
+
+        autoUpdater.on('update-not-available', info => {
+            mainWindow.webContents.send('update-not-available', info);
+        });
+
+        autoUpdater.on('error', err => {
+            mainWindow.webContents.send('update-error', err);
+        });
+
+        autoUpdater.on('update-downloaded', (downloadedFile, manifest, mandatory) => {
+            mainWindow.webContents.send('update-downloaded', downloadedFile, manifest, mandatory);
+        });
+
         if (!isDevEnv) {
-            setTimeout(() => autoUpdater.checkForUpdates(), 3000);
-            setInterval(() => autoUpdater.checkForUpdates(), 60 * 60 * 1000);
+            autoUpdater.didLastUpdateFail().then(failed => {
+                if (!failed) {
+                    setTimeout(() => autoUpdater.checkForUpdates(), 3000);
+                    setInterval(() => autoUpdater.checkForUpdates(), 60 * 60 * 1000);
+                }
+            });
         } else {
-            sendStatusToWindow('Updater did not start because dev build was detected.');
+            sendStatusToMainWindow('Updater did not start because dev build was detected.');
         }
     } catch (ex) {
-        sendStatusToWindow('Error starting updater.');
-        sendStatusToWindow(JSON.stringify(ex));
+        console.error('Error starting updater', ex);
+        sendStatusToMainWindow('Error starting updater.');
+        sendStatusToMainWindow(String(ex));
     }
 }
-
-autoUpdater.on('checking-for-update', () => {
-    sendStatusToWindow('Checking for update...');
-});
-
-autoUpdater.on('update-available', info => {
-    sendStatusToWindow('Update available.');
-    sendStatusToWindow(JSON.stringify(info));
-    window.webContents.send('warning', 'title_updateDownloading');
-});
-
-autoUpdater.on('update-not-available', info => {
-    sendStatusToWindow('Update not available.');
-    sendStatusToWindow(JSON.stringify(info));
-});
-
-autoUpdater.on('error', err => {
-    sendStatusToWindow('Error in auto-updater.');
-    sendStatusToWindow(err instanceof Error ? err.toString() : JSON.stringify(err));
-});
-
-autoUpdater.on('download-progress', progressObj => {
-    sendStatusToWindow('Download progress...');
-    sendStatusToWindow(JSON.stringify(progressObj));
-});
-
-autoUpdater.on('update-downloaded', info => {
-    sendStatusToWindow('Update downloaded.');
-    sendStatusToWindow(JSON.stringify(info));
-    window.webContents.send('update-will-restart');
-});
 
 module.exports = { start };

src/main.js
@@ -79,11 +79,6 @@ if (isDevEnv) {
     app.setPath('userData', path.resolve(app.getPath('appData'), `${app.getName().toLowerCase()}_dev`));
 }
 
-// <UPDATES> -----------------------------------------------------------------------------------------------------
-// If the app was started as a part of update process we don't want to proceed with startup
-if (require('~/main-process/handle-windows-update')) app.quit();
-// </UPDATES> ----------------------------------------------------------------------------------------------------
-
 // configure logging
 require('~/helpers/logging');
 

src/stores/language-store.js
@@ -83,7 +83,11 @@ class LanguageStore {
 
     @action loadSavedLanguage() {
         db.system.getValue('language')
-            .then(lang => this.changeLanguage(lang || 'en'));
+            .then(lang => this.changeLanguage(lang || 'en'))
+            .catch(err => {
+                console.error(err);
+                this.changeLanguage('en'); // still change to English
+            });
     }
 }
 

commit 683e98194fe47479d2c9336f1869e5aa33b287e7

fix: set translator replacements before setting language (#340)
Broken by PR #287

src/config.js
@@ -5,7 +5,7 @@ const app = require('electron').app || require('electron').remote.app;
 const isDevEnv = require('~/helpers/is-dev-env');
 const FileStream = require('peerio-icebear/dist/models/files/node-file-stream');
 const StorageEngine = require('peerio-icebear/dist/models/storage/node-json-storage');
-const { setUrlMap, setTagHandler } = require('peerio-translator');
+const { setUrlMap, setTagHandler, setStringReplacement } = require('peerio-translator');
 const tagHandlers = require('~/ui/shared-components/translator-tag-handlers');
 
 const packageJson = require(path.join(app.getAppPath(), 'package.json'));
@@ -27,6 +27,11 @@ setUrlMap(cfg.translator.urlMap);
 for (const name in tagHandlers) {
     setTagHandler(name, tagHandlers[name]);
 }
+
+// replace config-specific strings
+cfg.translator.stringReplacements.forEach((replacementObject) => {
+    setStringReplacement(replacementObject.original, replacementObject.replacement);
+});
 
 // --- PLATFORM SPECIFIC IMPLEMENTATIONS
 cfg.FileStream = FileStream;

src/ui/Root.jsx
@@ -1,7 +1,5 @@
 const React = require('react');
 const isDevEnv = require('~/helpers/is-dev-env');
-const config = require('~/config');
-const { setStringReplacement } = require('peerio-translator');
 const { Button, ProgressBar } = require('peer-ui');
 const DropTarget = require('./shared-components/DropTarget');
 const { ipcRenderer } = require('electron');
@@ -47,10 +45,6 @@ class Root extends React.Component {
             appState.devModeEnabled = true;
         }
 
-        // replace config-specific strings
-        config.translator.stringReplacements.forEach((replacementObject) => {
-            setStringReplacement(replacementObject.original, replacementObject.replacement);
-        });
         // events from main process
         ipcRenderer.on('warning', (ev, key) => warnings.add(key)); // TODO: not needed anymore?
         ipcRenderer.on('console_log', (ev, arg) => console.log(arg));
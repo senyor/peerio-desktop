ea1713c44d01c9ee959d648cc3db692a694fb789
Lucas <suprko@users.noreply.github.com>
2018-07-23 17:48:26 -0400

    feat: PendingDM small UI update (#362)
    * feat: small 'invite type' addition to UI of pending DM
    
    * chore: icebear branch
    [branch ch6131]
    
    * fix: only show invitation method for inviter, not invitee
    
    * chore: switch to dev icebear

--- src/ui/chat/components/PendingDMHeader.jsx
+++ src/ui/chat/components/PendingDMHeader.jsx
@@ -1,5 +1,6 @@
 const React = require('react');
 const { observer } = require('mobx-react');
+const { chatStore } = require('peerio-icebear');
 const T = require('~/ui/shared-components/T');
 
 const AvatarWithPopup = require('~/ui/contact/components/AvatarWithPopup');
@@ -10,18 +11,29 @@ const IdentityVerificationNotice = require('~/ui/chat/components/IdentityVerific
 class PendingDMHeader extends React.Component {
     render() {
         const c = this.props.contact;
+        const chat = chatStore.activeChat;
+
         return (
             <div className="pending-dm-header">
                 <EmojiImage emoji="tada" size="large" />
 
-                <T className="main-text"
-                    k={this.props.isNewUser
-                        ? 'title_newUserDmInviteHeading'
-                        : 'title_dmInviteHeading'
-                    }
-                >
-                    {{ contactName: c.fullName }}
-                </T>
+                {this.props.isNewUser
+                    ? <T k="title_newUserDmInviteHeading" className="main-text">{{ contactName: c.fullName }}</T>
+                    : <div className="main-text">
+                        <T k="title_goodNews" /><br />
+                        <T k="title_dmInviteHeading">{{ contactName: c.fullName }}</T>
+                    </div>
+                }
+
+                {this.props.isNewUser
+                    ? null
+                    : <T
+                        k={chat.isAutoImport ? 'title_userInAddressBook' : 'title_invitedUserViaEmail'}
+                        className="invite-type"
+                    >
+                        {{ firstName: c.firstName, email: chat.email }}
+                    </T>
+                }
 
                 <div className="user-profile-container">
                     <AvatarWithPopup contact={c} size="large" />

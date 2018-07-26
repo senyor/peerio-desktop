04db0a1446fcda888dbe6d2cfd8fe9ae32a509f5
suprko <lucas@peerio.com>
2018-07-26 11:20:08 -0400

    WIP: add progress bar and file error state

--- src/ui/files/components/FileStatusWindow.jsx
+++ src/ui/files/components/FileStatusWindow.jsx
@@ -4,9 +4,10 @@ const { observer } = require('mobx-react');
 
 const css = require('classnames');
 const T = require('~/ui/shared-components/T');
-const { Button, MaterialIcon } = require('peer-ui');
+const { Button, MaterialIcon, ProgressBar } = require('peer-ui');
 const FileSpriteIcon = require('~/ui/shared-components/FileSpriteIcon');
 
+// Just for testing
 const FILES = [
     { name: 'file.jpg', iconType: 'img' },
     { name: 'file.jpg', iconType: 'img' },
@@ -23,44 +24,47 @@ const FILES = [
 @observer
 class FileStatusWindow extends React.Component {
     @observable collapsed = false;
+    @observable allCompleted = true;
 
-    @action.bound onToggle() {
+    @action.bound toggleWindow() {
         this.collapsed = !this.collapsed;
     }
 
-    @computed get downloadQueue() {
-        return FILES.map(f => this.fileItem(f, 'download'));
-    }
-
-    @computed get uploadQueue() {
-        return FILES.map(f => this.fileItem(f, 'upload'));
-    }
+    @computed get downloadQueue() { return FILES.map(f => this.fileItem(f, 'download')); }
+    @computed get uploadQueue() { return FILES.map(f => this.fileItem(f, 'upload')); }
 
     fileItem(file, actionType) {
         const cancelFunction = `onCancel${actionType[0].toUpperCase() + actionType.slice(1)}`;
 
         return (
-            <div className="file-item">
+            <div className={css('file-item', {error: file.error })}>
                 <FileSpriteIcon type={file.iconType} size="small" />
                 <span className="file-name">{file.name}</span>
 
-                {file.completed
-                    ? (
-                        <span className="right-icon">
-                            <MaterialIcon className="completed" icon="check" />
-                            <Button
-                                className="find-file"
-                                icon="search"
-                                theme="small"
-                                onClick={() => this.findFile(file)}
-                            />
-                        </span>
-                    )
-                    : <Button
-                        className="right-icon"
-                        icon="highlight_off"
-                        onClick={this[cancelFunction]}
-                    />
+                {file.error
+                    ? <MaterialIcon icon="error_outline" className="right-icon error" />
+                    : file.completed
+                        ? (
+                            <span className="right-icon">
+                                <MaterialIcon className="completed" icon="check" />
+                                <Button
+                                    className="find-file"
+                                    icon="search"
+                                    theme="small"
+                                    onClick={() => this.findFile(file)}
+                                />
+                            </span>
+                        )
+                        : <Button
+                            className="right-icon"
+                            icon="highlight_off"
+                            onClick={this[cancelFunction]}
+                        />
+                }
+
+                {file.progress
+                    ? <ProgressBar value={50} max={100} />
+                    : null
                 }
             </div>
         );
@@ -74,16 +78,30 @@ class FileStatusWindow extends React.Component {
         console.log('cancel upload');
     }
 
+    findFile(file) {
+        console.log('find file');
+    }
+
     render() {
         return (
             <div className={css('file-status-window', { collapsed: this.collapsed })}>
                 <div className="title-bar">
                     <T k="title_fileStatus">{{ number: 43 }}</T>
-                    <Button
-                        icon={this.collapsed ? 'keyboard_arrow_up' : 'keyboard_arrow_down'}
-                        onClick={this.onToggle}
-                        theme="small"
-                    />
+                    <div className="buttons-container">
+                        <Button
+                            icon={this.collapsed ? 'keyboard_arrow_up' : 'keyboard_arrow_down'}
+                            onClick={this.toggleWindow}
+                            theme="small"
+                        />
+                        {this.allCompleted
+                            ? <Button
+                                icon="close"
+                                onClick={this.closeWindow}
+                                theme="small"
+                            />
+                            : null
+                        }
+                    </div>
                 </div>
                 <div className="body">
                     <div className="heading">

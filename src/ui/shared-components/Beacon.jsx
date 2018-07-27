821def8be22e8e8c5a90e4899cc220287f67e2c3
suprko <lucas@peerio.com>
2018-07-27 14:53:02 -0400

    chore: comment

--- src/ui/shared-components/Beacon.jsx
+++ src/ui/shared-components/Beacon.jsx
@@ -4,9 +4,20 @@ const React = require('react');
 class Beacon extends React.Component {
     render() {
         return (
+            /*
+                A container wraps the child content and beacon together, to make positioning easy.
+                This <div> nesting is a necessary eyesore to allow the container (and therefore the child content) to
+                be positioned however is necessary in the view (absolute, fixed, whatever), while the inner content
+                can be positioned `relative`, so that the beacon itself can be positioned `absolute`. Whew!
+
+                All of this means that the beacon positioning will be accurate to the child content without expensive
+                in-window calculations.
+            */
             <div className="beacon-container">
                 <div className="beacon-inner">
                     {this.props.children}
+
+                    <div className="beacon" />
                 </div>
             </div>
         );

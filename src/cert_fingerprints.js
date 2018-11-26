e69b25ea3bc491f735143170fc59a79451aceba9
Dmitry Chestnykh <dmitry@codingrobots.com>
2018-11-26 17:23:01 +0100

    chore: add new cert fingerprint (#502)

--- src/cert_fingerprints.js
+++ src/cert_fingerprints.js
@@ -2,5 +2,9 @@ export default [
     {
         hostRegex: /peerio\.com$/gi,
         fingerprint: 'sha256/E9eOXAcJGIUwjIxAepQ4cH7SrNoWcoCrS/n207eFReA='
+    },
+    {
+        hostRegex: /peerio\.com$/gi,
+        fingerprint: 'sha256/J7PPu+NdfdSEM/D9ORin3OMAHdC2xgEg3fQ/+fhaDzs='
     }
 ];

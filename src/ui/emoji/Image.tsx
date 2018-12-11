e6b7fb675d76e14a3d604241cf215d2cbb441843
Anri Asaturov <anri82@gmail.com>
2018-12-11 21:41:37 +0200

    fix: EmojiImage path

--- src/ui/emoji/Image.tsx
+++ src/ui/emoji/Image.tsx
@@ -13,13 +13,6 @@ interface EmojiImageProps {
 /**
  * This component returns an individual image file of an emoji, rather than
  * using the spritesheets.
- *
- * Note: if you set the `size` prop to `large`, it will use the 2x version of
- * the image. The 2x files are not all in the static folder! Because of how
- * infrequently we use this component, we just copy those in individually as we
- * use them. So, if you need a 2x version of an emoji that isn't in the folder
- * (static/emoji/png/2x/), you need to copy it from
- * node_modules/emojione-assets/
  */
 @observer
 export default class EmojiImage extends React.Component<EmojiImageProps> {
@@ -28,14 +21,7 @@ export default class EmojiImage extends React.Component<EmojiImageProps> {
             <img
                 className={css('emoji-image', this.props.size, this.props.className)}
                 title={this.props.emoji}
-                src={
-                    this.props.size === 'large'
-                        ? emojiByCanonicalShortname[`:${this.props.emoji}:`].filename.replace(
-                              '/png/',
-                              '/png/2x/'
-                          )
-                        : emojiByCanonicalShortname[`:${this.props.emoji}:`].filename
-                }
+                src={emojiByCanonicalShortname[`:${this.props.emoji}:`].filename}
             />
         );
     }
